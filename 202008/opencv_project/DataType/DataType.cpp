810408cebb1572e3c8b0da30fc9cd71c4bd83f6f
sk95120 <765871277@qq.com>
2020-08-13 03:29:41 -0700

    1. XOR Not And ,logic operate;

--- 202008/opencv_project/DataType/DataType.cpp
+++ 202008/opencv_project/DataType/DataType.cpp
@@ -213,8 +213,39 @@ void LookUpTable () {
 	waitKey(0);
 }
 
+/**
+ * LogicOperation
+ * **/
+void LogicOperation () {
+	Mat img = imread("/home/shike/Pictures/lenna_head_peng_type.png");
+	if (img.empty()) {
+                 cout << "picture file empty." << endl;
+         }
+	Mat img0 = Mat::zeros(200 ,200 ,CV_8UC1);
+	Mat img1 = Mat::zeros(200 ,200 ,CV_8UC1);
+	Rect rect0(50 ,50 ,100 ,100);
+	img0(rect0) = Scalar(255);
+	Rect rect1(100 ,100 ,100 ,100);
+	img1(rect1) = Scalar(255);
+	imshow("img0" ,img0);
+	imshow("img1" ,img1);
+
+	Mat myAnd ,myOr ,myXor ,myNot ,imgNot;
+	bitwise_not(img0 ,myNot);
+	bitwise_and(img0 ,img1 ,myAnd);
+	bitwise_or(img0 ,img1 ,myOr);
+	bitwise_xor(img0 ,img1 ,myXor);
+	bitwise_not(img ,imgNot);
+	imshow("myAnd" ,myAnd);
+	imshow("myOr" ,myOr);
+	imshow("myXor" ,myXor);
+	imshow("myNot" ,myNot);
+	imshow("imgNot" ,imgNot);
+	waitKey(0);
+}
+
 int main() {
-	LookUpTable();
+	LogicOperation();
 	return 0;
 }
 
