17845deb6b0718258de8918258a918c23f897982
sk95120 <765871277@qq.com>
2020-09-02 20:02:28 +0800

    fix bug: 1. compile error ;

--- 202008/ffmpeg_project/demux_decode/packet_demux_decode.c
+++ 202008/ffmpeg_project/demux_decode/packet_demux_decode.c
@@ -1,5 +1,5 @@
 #include <libavutil/imgutils.h>
-#include <libavutil/smaplefmt.h>
+#include <libavutil/samplefmt.h>
 #include <libavutil/timestamp.h>
 #include <libavformat/avformat.h>
 
@@ -14,7 +14,7 @@ static const char *audio_dst_filename = NULL;
 static FILE *video_dst_file = NULL;
 static FILE *audio_dst_file = NULL;
 
-static uint8_t *video_dst_data[4] = {NULL}
+static uint8_t *video_dst_data[4] = {NULL};
 static int 	video_dst_linesize[4];
 static int video_dst_bufsize;
 
@@ -29,9 +29,9 @@ static int audio_frame_count = 0;
 static int refcount = 0;
 
 
-static int decode_packet(int *got_frame .int cached) {
+static int decode_packet(int *got_frame ,int cached) {
 	int ret = 0;
-	inr decoded = pkt.size;	
+	int decoded = pkt.size;	
 	*got_frame = 0;
 	
 	if (pkt.stream_index == video_stream_idx) {
@@ -54,8 +54,8 @@ static int decode_packet(int *got_frame .int cached) {
 					cached ? "(cached)" :"" ,video_frame_count++ ,frame->coded_picture_number );
 
 			/** copy decoded frame to destination buffer:this is required since rawvideo expexts non aligned data **/
-			av_iamge_copy(video_dst_data ,video_dst_linesize 
-					,(const uint8_t **)(frame->datta) ,frame->linesize
+			av_image_copy(video_dst_data ,video_dst_linesize 
+					,(const uint8_t **)(frame->data) ,frame->linesize
 					,pix_fmt ,width ,height);
 			fwrite(video_dst_data[0] ,1 ,video_dst_bufsize ,video_dst_file);
 		}
@@ -71,7 +71,7 @@ static int decode_packet(int *got_frame .int cached) {
 		 * Smaple:fate-suit/lossless-audio/luckynight-partial.shn  Also, some decoders might over-read the packet.**/
 		decoded = FFMIN(ret ,pkt.size);
 		if (*got_frame) {
-			size_t unpadded_linesize = frame->nb_samples * av_get_byte_per_sample(frame->format);
+			size_t unpadded_linesize = frame->nb_samples * av_get_bytes_per_sample(frame->format);
 			printf("audio_frame %s n:%d nb_samples:%d pts:%s\n"
 					,cached ? "(cached)" : "" ,audio_frame_count++ ,frame->nb_samples ,av_ts2timestr(frame->pts ,&audio_dec_ctx->time_base));
 			/** Write the raw audio data samples of the first plane. This works fine for packed formats (e.g. AV_SAMPLE_FMR_S16). However, most audio decoders output planar audio ,which uses a separate plane of audio samples for each channel(e.g. AV_SAMPLE_FMT_S16S).
@@ -94,11 +94,12 @@ static int open_codec_context(int *stream_idx ,AVCodecContext **dec_ctx
 		,AVFormatContext *fmt_ctx ,enum AVMediaType type) {
 	int ret ,stream_index;
 	AVStream *st;
+	AVCodec *dec = NULL;
 	AVDictionary *opts = NULL;
 
 	ret = av_find_best_stream(fmt_ctx ,type ,-1 ,-1 ,NULL ,0);
 	if (ret < 0) {
-		fprintf(stderr ,"Could not find %d stream in input file %s",
+		fprintf(stderr ,"Could not find %s stream in input file %s",
 				av_get_media_type_string(type) ,src_filename);
 		return ret;
 	} else {
@@ -117,7 +118,7 @@ static int open_codec_context(int *stream_idx ,AVCodecContext **dec_ctx
 		if (!*dec_ctx) {
 			fprintf(stderr ,"Failed to allocate the %s codec context\n"
 					,av_get_media_type_string(type));
-			return AVERROR(ENOMEN);
+			return AVERROR(ENOMEM);
 		}
 
 		/** Copy codec parameters from input stream to output codec context **/
@@ -137,6 +138,32 @@ static int open_codec_context(int *stream_idx ,AVCodecContext **dec_ctx
 		*stream_idx = stream_index;
 	}
 	return 0;
+}
+
+static int get_format_from_sample_fmt(const char **fmt ,enum AVSampleFormat sample_fmt) {
+	
+	int i;
+	struct sample_fmt_entry {
+		enum AVSampleFormat sample_fmt;
+		const char *fmt_be ,*fmt_le;
+	} sample_fmt_entries[] = {
+		{AV_SAMPLE_FMT_U8 ,"u8" ,"u8"},
+		{AV_SAMPLE_FMT_S16 ,"s16be" ,"s16le"},
+		{AV_SAMPLE_FMT_S32 ,"s32be" ,"s32le"},
+		{AV_SAMPLE_FMT_FLT ,"f32be" ,"f32le"},
+		{AV_SAMPLE_FMT_DBL ,"f64be" ,"f64le"}
+	};
+	*fmt = NULL;
+	for (i = 0 ; i < FF_ARRAY_ELEMS(sample_fmt_entries) ; i++) {
+		struct sample_fmt_entry *entry = &sample_fmt_entries[i];
+		if (sample_fmt == entry->sample_fmt) {
+			*fmt = AV_NE(entry->fmt_be ,entry->fmt_le);
+			return 0;
+		}
+	}
+	fprintf(stderr ,"sample format %s is not supported as output format\n"
+			,av_get_sample_fmt_name(sample_fmt));
+	return -1;
 }
 
 int main(int argc ,char **argv) {
@@ -181,7 +208,7 @@ int main(int argc ,char **argv) {
 		pix_fmt = video_dec_ctx->pix_fmt;
 		ret = av_image_alloc(video_dst_data ,video_dst_linesize ,width ,height ,pix_fmt ,1);
 		if (ret < 0) {
-			fpritnf(stderr ,"Could not allocate raw video buffer\n");
+			fprintf(stderr ,"Could not allocate raw video buffer\n");
 			goto end;
 		}
 		video_dst_bufsize = ret;
@@ -209,8 +236,8 @@ int main(int argc ,char **argv) {
 
 	frame = av_frame_alloc();
 	if (!frame) {
-		fprintf(stderr ."Could not allocate frame\n");
-		ret = AVERROR(ENOMEN);
+		fprintf(stderr ,"Could not allocate frame\n");
+		ret = AVERROR(ENOMEM);
 		goto end;
 	}
 
@@ -237,7 +264,7 @@ int main(int argc ,char **argv) {
 			}
 			pkt.data += ret;
 			pkt.size -= ret;
-		} while(pkt.size > 0)
+		} while (pkt.size > 0);
 		av_packet_unref(&orig_pkt);
 	}
 
@@ -260,20 +287,20 @@ int main(int argc ,char **argv) {
 			const char *packed = av_get_sample_fmt_name(sfmt);
 			printf("Warning: the sample format the decoder produced is planner (%s). This sample will output the first channel only.\n"
 					,packed ? packed : "?");
-			sfmt = av_get_packed_smaple_fmt(sfmt);
+			sfmt = av_get_packed_sample_fmt(sfmt);
 			n_channels = 1;
 		}
+		if ((ret = get_format_from_sample_fmt(&fmt ,sfmt)) < 0) {
+			goto end;
+		}
+		printf("Play the output audio file with the command:\n ffplay -f %s -ac %d -ar %d %s\n",
+				fmt, n_channels ,audio_dec_ctx->sample_rate ,audio_dst_filename);
 	}
-	if ((ret = get_format_from_sample_fmt(&fmt ,sfmt)) < 0) {
-		goto end;
-	}
-	printf("Play the output audio file with the command:\n ffplay -f %s -ac %d -ar %d %s\n",
-			fmt, n_channels ,audio_dec_ctx->sample_rate ,audio_dst_filename);
 
 end:
 	avcodec_free_context(&video_dec_ctx);
 	avcodec_free_context(&audio_dec_ctx);
-	avdformat_close_input(&fmt_ctx);
+	avformat_close_input(&fmt_ctx);
 	if (video_dst_file) {
 		fclose(video_dst_file);
 	}
