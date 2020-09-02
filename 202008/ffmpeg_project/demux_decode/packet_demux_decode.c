65f2bc8ba2d412794cbaa2b45e4ce3ea928a5a94
sk95120 <765871277@qq.com>
2020-09-02 18:04:32 +0800

    1. a process of demux and decode media to rawvideo and rawaudio;

--- 202008/ffmpeg_project/demux_decode/packet_demux_decode.c
+++ 202008/ffmpeg_project/demux_decode/packet_demux_decode.c
@@ -138,3 +138,150 @@ static int open_codec_context(int *stream_idx ,AVCodecContext **dec_ctx
 	}
 	return 0;
 }
+
+int main(int argc ,char **argv) {
+	
+	int ret = 0 ,got_frame;
+	if (argc != 4 && argc != 5) {
+		fprintf(stderr ,"Usage : %s [-refcount] input_file video_output_file audio_output_file\n API example program to show how to read frames from an input file.\n This program reads frames from a file, decode them, and writes decoded video frame to a rawvideo file named video_output_file ,and decoded audio frames to a rawaudio file named audio_output_file.\n\n If the -refcount option is specified, the program use the refrence counting frames system which allows keeping a copy of the data for longer than one decode call.\n\n" ,argv[0]);
+		exit(1);
+	}
+
+	if (argc == 5 && !strcmp(argv[1] , "-refcount")) {
+		refcount = 1;
+		argv ++;
+	}
+	src_filename = argv[1];
+	video_dst_filename = argv[2];
+	audio_dst_filename = argv[3];
+
+	/** Open input file, and allocate format context **/
+	if (avformat_open_input(&fmt_ctx ,src_filename ,NULL ,NULL) < 0) {
+		fprintf(stderr ,"Could not open source file %s\n"
+				,src_filename);
+		exit(1);
+	}
+
+	/** retrieve stream information **/
+	if (avformat_find_stream_info(fmt_ctx ,NULL) < 0) {
+		fprintf(stderr ,"Could not find stream informatin.\n");
+		exit(1);
+	}
+	if (open_codec_context(&video_stream_idx ,&video_dec_ctx ,fmt_ctx ,AVMEDIA_TYPE_VIDEO) > 0) {
+		video_stream = fmt_ctx->streams[video_stream_idx];
+		if (!video_dst_file) {
+			fprintf(stderr ,"Could not open destination file %s\n"
+					,video_dst_filename);
+			ret = 1;
+			goto end;
+		}
+		/** allocate image where the decoded image will be put **/
+		width = video_dec_ctx->width;
+		height = video_dec_ctx->height;
+		pix_fmt = video_dec_ctx->pix_fmt;
+		ret = av_image_alloc(video_dst_data ,video_dst_linesize ,width ,height ,pix_fmt ,1);
+		if (ret < 0) {
+			fpritnf(stderr ,"Could not allocate raw video buffer\n");
+			goto end;
+		}
+		video_dst_bufsize = ret;
+	}
+
+	if (open_codec_context(&audio_stream_idx ,&audio_dec_ctx ,fmt_ctx ,AVMEDIA_TYPE_AUDIO) >= 0) {
+		audio_stream = fmt_ctx->streams[audio_stream_idx];
+		audio_dst_file = fopen(audio_dst_filename, "wb");
+		if (!audio_dst_file) {
+			fprintf(stderr ,"Could not open destination file %s\n"
+					,audio_dst_filename);
+			ret = 1;
+			goto end;
+		}
+	}
+
+	/** dump input information to stderr **/
+	av_dump_format(fmt_ctx ,0 ,src_filename ,0);
+
+	if (!audio_stream && !video_stream) {
+		fprintf(stderr ,"Could not find audio or video stream in the input, aborting\n");
+		ret = 1;
+		goto end;
+	}
+
+	frame = av_frame_alloc();
+	if (!frame) {
+		fprintf(stderr ."Could not allocate frame\n");
+		ret = AVERROR(ENOMEN);
+		goto end;
+	}
+
+	/** initialize packet, set data to NULL, let the demuxer fill it **/
+	av_init_packet(&pkt);
+	pkt.data = NULL;
+	pkt.size = 0;
+
+	if (video_stream) {
+		printf("Demuxing video from file '%s' into '%s' \n"
+				,src_filename ,audio_dst_filename);
+	}
+	if (audio_stream) {
+		printf("Demuxing audio from file '%s' into '%s'\n "
+				,src_filename ,audio_dst_filename);
+	}
+	/** read frames from the file **/
+	while (av_read_frame(fmt_ctx, &pkt) >= 0) {
+		AVPacket orig_pkt = pkt;
+		do {
+			ret = decode_packet(&got_frame ,0);
+			if (ret < 0) {
+				break;
+			}
+			pkt.data += ret;
+			pkt.size -= ret;
+		} while(pkt.size > 0)
+		av_packet_unref(&orig_pkt);
+	}
+
+	/** flush cached frames **/
+	pkt.data = NULL;
+	pkt.size = 0;
+	do {
+		decode_packet(&got_frame ,1);
+	} while (got_frame);
+	printf("Demuxing succeeded.\n");
+	if (video_stream) {
+		printf("Paly the output file with the command:\n ffplay -f rawvideo -pix_fmt %s -video_size %dx%d %s \n"
+				,av_get_pix_fmt_name(pix_fmt) ,width ,height ,video_dst_filename);
+	}
+	if (audio_stream) {
+		enum AVSampleFormat sfmt = audio_dec_ctx->sample_fmt;
+		int n_channels = audio_dec_ctx->channels;
+		const char *fmt;
+		if (av_sample_fmt_is_planar(sfmt)) {
+			const char *packed = av_get_sample_fmt_name(sfmt);
+			printf("Warning: the sample format the decoder produced is planner (%s). This sample will output the first channel only.\n"
+					,packed ? packed : "?");
+			sfmt = av_get_packed_smaple_fmt(sfmt);
+			n_channels = 1;
+		}
+	}
+	if ((ret = get_format_from_sample_fmt(&fmt ,sfmt)) < 0) {
+		goto end;
+	}
+	printf("Play the output audio file with the command:\n ffplay -f %s -ac %d -ar %d %s\n",
+			fmt, n_channels ,audio_dec_ctx->sample_rate ,audio_dst_filename);
+
+end:
+	avcodec_free_context(&video_dec_ctx);
+	avcodec_free_context(&audio_dec_ctx);
+	avdformat_close_input(&fmt_ctx);
+	if (video_dst_file) {
+		fclose(video_dst_file);
+	}
+	if (audio_dst_file) {
+		fclose(audio_dst_file);
+	}
+	av_frame_free(&frame);
+	av_free(video_dst_data[0]);
+
+	return ret < 0;
+}
