2722f88562277ab819aea21667e7b1baf94154c3
sk95120 <765871277@qq.com>
2020-09-06 06:54:17 -0700

    1. add reference hpp file;

--- 202008/ffmpeg_project/demux_decode/packet_video_filter.c
+++ 202008/ffmpeg_project/demux_decode/packet_video_filter.c
@@ -1,3 +1,19 @@
- 
- 
+#define _XOPEN_SOURCE 600 /** for thread usleep **/
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <linavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+#include <liavutil/opt.h>
+
+const char *filter_descr = "scale=78:24,transpose=cclock"
+/** other way:
+ *  scale=78:24[scl]:[scl] transpose=cclock 
+ *  //assumes "[in]" and "[out]" to be input output pads respectively
+ * **/
+
+
 
