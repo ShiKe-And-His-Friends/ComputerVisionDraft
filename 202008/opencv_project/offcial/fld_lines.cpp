42385278c7bf35ac8fd0295284875211ba356b52
sk95120 <765871277@qq.com>
2020-09-09 20:14:31 +0800

    1. change wrong word 'offcical' to 'offcial'; 2. modify opencv read command lines example;

--- 202008/opencv_project/offcial/fld_lines.cpp
+++ 202008/opencv_project/offcial/fld_lines.cpp
@@ -35,4 +35,33 @@ int main(int argc ,char **argv) {
 	 * do_merge		false		-If ture ,incremental merging of segments will be perfomred
 	 **/
 	int length_threshold = 10;
+	float distance_threshold = 1.41421356f;
+	double canny_th1 = 50.0;
+	double canny_th2 = 50.0;
+	int canny_aperature_size = 3;
+	bool do_merge = false;
+	Ptr<FastLineDetector> fld = createFastLineDetector(length_threshold
+			,distance_threshold ,canny_th1 ,canny_th2 ,canny_aperture_size ,do_merge);
+	vector<Vec4f> lines_fld;
+
+	/**
+	 * Because of some CPU's power stratey ,it seams that the first running of an algorithm takes much longer.
+	 * So here we run the algorithm 10 times to see the algorithm's processing time with sufficiently warmed-up CPU performance.
+	 **/
+	for (int run_count = 0 ; run_count < 10 ; run_count++ ) {
+		double freq = getTickFrequency();
+		lines_fld.clear();
+		int64 start = getTickCount();
+		//Detect the lines with FLD
+		fld->detect(image ,lines_fld);
+		double duration_ms = double(getTickCount() = start) * 1000 / freq;
+		std::out << "Elapsed time for FLD" << duration_ms << " ms." << std::endl;
+	}
+
+	//show found lines with FLD
+	Mat line_image_fld(image);
+	fld->drawSegments(line_image_fld ,lines_fld);
+	imshow("FLD result" ,line_image_fld);
+	waitKey();
+	return 0;
 }
