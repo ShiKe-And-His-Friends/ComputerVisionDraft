20c510eaa09db1e1693abacb517dfd2ffde24065
sk95120 <765871277@qq.com>
2020-09-10 12:27:48 +0800

    1. commit some lines code;

--- 202008/ffmpeg_project/demux_decode/packet_audio_filter_before.c
+++ 202008/ffmpeg_project/demux_decode/packet_audio_filter_before.c
@@ -30,7 +30,7 @@ static int init_filter_graph(AVFilterGraph **graph ,AVFilterContext **src ,AVFIl
 	AVFilterContext *abuffersink_ctx;
 	const AVFilter *abuffersink;
 
-	AVDictionary *options_disc = NULL;
+	AVDictionary *options_dist = NULL;
 	uint8_t options_str[1024];
 	uint8_t ch_layout[64];
 
@@ -54,6 +54,57 @@ static int init_filter_graph(AVFilterGraph **graph ,AVFilterContext **src ,AVFIl
 		fprintf(stderr ,"Could not allocate the abuffer instance.\n");
 		return AVERROR(ENOMEM);
 	}
+	/** Set the filter options through the AVOptions API. **/
+	av_get_channel_layout_string(ch_layout ,sizeof(ch_layout) ,0 ,INPUT_CHANNEL_LAYOUT);
+	av_opt_set(abuffer_ctx ,"channel_layout" ,ch_layout ,AV_OPT_SEARCH_CHILDREN);
+	av_opt_set(abuffer_ctx ,"sample_fmt" ,av_get_sample_fmt_name(INPUT_FORMAT) ,AV_OPT_SEARCH_CHILDREN);
+	av_opt_set_q(abuffer_ctx ,"time_base" , (AVRational){1 ,INPUT_SAMPLERATE} ,AV_OPT_SEARCH_CHILDREN);
+	av_opt_set_int(abuffer_ctx ,"sample_rate" ,INPUT_SAMPLERATE ,AV_OPT_SEARCH_CHILDREN);
+
+	/** Now initialize the filter; we pass NULL options ,since we have already set all options above **/
+	err = avfilter_init_str(abuffer_ctx ,NULL);
+	if (err < 0) {
+		fprintf(stderr ,"Could not initialize the abuffer filter\n");
+		return err;
+	}
+
+	/** Create volume filter **/
+	volume = avfilter_get_by_name("volume");
+	if (!volume) {
+		fprintf(stderr ,"Could not find the volume filter.\n");
+		return AVERROR_FILTER_NOT_FOUND;
+	}
+
+	volume_ctx = avfilter_graph_alloc_filter(filter_graph ,volume ,"volume");
+	if (!volume_ctx) {
+		fprintf(stderr ,"Could not allocate the volume instance.\n");
+		return AVERROR(ENOMEM);
+	}
+
+	/** A different way of passing the options is as key/value pairs in a dictionary. **/
+	av_dict_set(&options_dist ,"volume" ,AV_STRINGIFY(VOLUME_VAL) ,0);
+	err = avfilter_init_dict(volume_ctx ,&options_dist);
+	av_dict_free(&options_dist);
+	if (err < 0) {
+		fprintf(stderr ,"Could not initialize the volume filter.\n");
+		return err;
+	}
+
+	/** Create the aformat filter; it ensures that the output is of the format we want **/
+	aformat = avfilter_get_by_name("aformat");
+	if (!aformat) {
+		fprintf(stderr ,"Could not find the aformat filter.\n");
+		return AVERROR_FILTER_NOT_FOUND;
+	}
+	aformat_ctx = avfilter_graph_alloc_filter(filter_graph ,aformat ,"aformat");
+	if (!aformat_ctx) {
+		fprintf(stderr ,"Could not allocate the aformat instance.\n");
+		return AVERROR(ENOMEM);
+	}
+
+	/** A third way of passing the options is in a string of the form key1=value1:key2=walue2... **/
+	sprintf(option_str ,sizeof(option_str) ,"sample_fmt=&s:sample_rates=%d:channel_layout=0x%"PRIx64 ,av_get_sample_fmt_name(AV_SAMPLE_FMT_S16) ,44100 ,(uint64_t)AV_CH_LAYOUT_STEREO);
+	err = avfilter_init_str(aform);
 
 
 }
