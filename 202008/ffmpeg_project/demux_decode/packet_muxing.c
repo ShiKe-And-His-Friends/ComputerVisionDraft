e68dcd319e5caa14d3dd767aa204a13c602d6f92
sk95120 <765871277@qq.com>
2020-09-17 21:04:37 +0800

    fix bug: 1. addStream() methods's core dump;

--- 202008/ffmpeg_project/demux_decode/packet_muxing.c
+++ 202008/ffmpeg_project/demux_decode/packet_muxing.c
@@ -57,6 +57,12 @@ static void add_stream(OutputStream *ost ,AVFormatContext *oc ,AVCodec **codec ,
 		fprintf(stderr ,"Could not find encoder for '%s' \n" ,avcodec_get_name(codec_id));
 		exit(1);
 	}
+
+	ost->st = avformat_new_stream(oc ,NULL);
+	if (!ost->st) {
+		fprintf(stderr ,"Could not allocate stream.\n");
+		exit(1);
+	}
 	ost->st->id = oc->nb_streams-1;
 	c = avcodec_alloc_context3(*codec);
 	if (!c) {
@@ -72,7 +78,7 @@ static void add_stream(OutputStream *ost ,AVFormatContext *oc ,AVCodec **codec ,
 			if ((*codec)->supported_samplerates) {
 				c->sample_rate = (*codec)->supported_samplerates[0];
 				for (i = 0 ;(*codec)->supported_samplerates[i] ; i++ ) {
-					if ((*codec)->supported_samplerates[i] == 4410) {
+					if ((*codec)->supported_samplerates[i] == 44100) {
 						c->sample_rate = 44100;
 					}
 				}
@@ -90,6 +96,7 @@ static void add_stream(OutputStream *ost ,AVFormatContext *oc ,AVCodec **codec ,
 			c->channels = av_get_channel_layout_nb_channels(c->channel_layout);
 			ost->st->time_base = (AVRational){1 ,c->sample_rate};
 			break;
+
 		case AVMEDIA_TYPE_VIDEO:
 			c->codec_id = codec_id;
 			c->bit_rate = 400000;
