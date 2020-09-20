6ec0394944043a59b84a2d4a4fe855b150e8f60b
sk95120 <765871277@qq.com>
2020-09-20 15:12:40 +0800

    fix bug: 1. remuxing compile wrong; 2. feature remuxing mpeg4 video horizontal into vertical;

--- 202008/ffmpeg_project/demux_decode/packet_remuxing.c
+++ 202008/ffmpeg_project/demux_decode/packet_remuxing.c
@@ -2,14 +2,14 @@
 #include <libavformat/avformat.h>
 
 static void log_packet(const AVFormatContext *fmt_ctx ,const AVPacket *pkt ,const char *tag) {
-	AVRational *time_base = &fmt_ctx->stream[pke->stream_index]->time_base;
+	AVRational *time_base = &fmt_ctx->streams[pkt->stream_index]->time_base;
 
-	printf("%s: pts: %s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d \n" ,tag ,av_ts2str(pkt->pts) ,av_ts2timerstr(pkt->pts) ,av_ts2timer(pkt->pts ,time_base) ,av_ts2str(pkt->dts) ,av_ts2timestr(pkt->dts ,time_base) ,av_ts2str(pkt->duration) ,av_ts2timestr(pkt->duration ,time_base) ,pkt->stream_index );
+	printf("%s: pts: %s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d \n" ,tag ,av_ts2str(pkt->pts) ,av_ts2timestr(pkt->pts ,time_base) ,av_ts2str(pkt->dts) ,av_ts2timestr(pkt->dts ,time_base) ,av_ts2str(pkt->duration) ,av_ts2timestr(pkt->duration ,time_base) ,pkt->stream_index );
 }
 
 int main(int argc ,char **argv) {
 
-	AVOutputFormat *fmt = NULL;
+	AVOutputFormat *ofmt = NULL;
 	AVFormatContext *ifmt_ctx = NULL ,*ofmt_ctx = NULL;
 	AVPacket pkt;
 	const char *in_filename, *out_filename;
@@ -35,7 +35,7 @@ int main(int argc ,char **argv) {
 		goto end;
 	}
 	av_dump_format(ifmt_ctx ,0 ,in_filename ,0);
-	av_format_alloc_output_context2(&ofmt_ctx ,NULL ,NULL ,out_filename);
+	avformat_alloc_output_context2(&ofmt_ctx ,NULL ,NULL ,out_filename);
 	if (!ofmt_ctx) {
 		fprintf(stderr ,"Could not create output context.\n");
 		ret = AVERROR_UNKNOWN;
@@ -52,26 +52,26 @@ int main(int argc ,char **argv) {
 
 	for (i = 0 ; i < ifmt_ctx->nb_streams ; i++) {
 		AVStream *out_streams;
-		AVStream *in_stream = ifmt_ctx->stream[i];
+		AVStream *in_stream = ifmt_ctx->streams[i];
 		AVCodecParameters *in_codecpar = in_stream->codecpar;
-		if (in_codecpar->codc_type != AVMEDIA_TYPE_AUDIO && in_codecpar->codec_type != AVMEDIA_TYPE_VIDEO && in_codecpar->codec_type != AVMEDIA_TYPE_SUBTITLE) {
+		if (in_codecpar->codec_type != AVMEDIA_TYPE_AUDIO && in_codecpar->codec_type != AVMEDIA_TYPE_VIDEO && in_codecpar->codec_type != AVMEDIA_TYPE_SUBTITLE) {
 			stream_mapping[i] = -1;
 			continue;
 		}
 
 		stream_mapping[i] = stream_index++;
-		out_stream = avformat_new_stream(ofmt_ctx ,NULL);
-		if (!out_stream) {
+		out_streams = avformat_new_stream(ofmt_ctx ,NULL);
+		if (!out_streams) {
 			fprintf(stderr ,"Failed allocating output stream.\n");
 			ret = AVERROR_UNKNOWN;
 			goto end;
 		}
-		ret = avcodec_parameters_copy(out_stream->codecpar ,in_codecpar);
+		ret = avcodec_parameters_copy(out_streams->codecpar ,in_codecpar);
 		if (ret < 0) {
 			fprintf(stderr ,"Failed to copy codec parameters.\n");
 			goto end;
 		}
-		out_stream->codecpar->codec_tag = 0;
+		out_streams->codecpar->codec_tag = 0;
 	}
 	av_dump_format(ofmt_ctx ,0 ,out_filename ,1);
 	if (!(ofmt->flags & AVFMT_NOFILE)) {
@@ -104,8 +104,8 @@ int main(int argc ,char **argv) {
 
 		/** copy packet **/
 		pkt.pts = av_rescale_q_rnd(pkt.pts ,in_stream->time_base ,out_stream->time_base ,AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
-		pkt.dts = av_rescale_q_rnd(pkt.dts ,in_stream->time_base ,out_stream->time_base ,AV_ROUND_NEAR_EOF|AV_ROUND_PASS_MINMAX);
-		pkt,duration = av_rescale_q(pkt.duration ,in_stream->time_base ,out_stream->time_base);
+		pkt.dts = av_rescale_q_rnd(pkt.dts ,in_stream->time_base ,out_stream->time_base ,AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
+		pkt.duration = av_rescale_q(pkt.duration ,in_stream->time_base ,out_stream->time_base);
 		pkt.pos = -1;
 		log_packet(ofmt_ctx ,&pkt ,"out");
 		ret = av_interleaved_write_frame(ofmt_ctx ,&pkt);
@@ -123,7 +123,7 @@ end:
 	if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE)) {
 		avio_closep(&ofmt_ctx->pb);
 	}
-	avforamt_free_context(ofmt_ctx);
+	avformat_free_context(ofmt_ctx);
 	av_freep(&stream_mapping);
 	if (ret < 0 && ret != AVERROR_EOF) {
 		fprintf(stderr ,"Error occurred: %s \n" ,av_err2str(ret));
