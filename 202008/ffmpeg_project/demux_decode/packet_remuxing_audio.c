8e9e418c84456fe7c6ee4d7c5321c8e03edfa84b
sk95120 <765871277@qq.com>
2020-09-22 19:27:21 +0800

    1. audio remuxing;

--- 202008/ffmpeg_project/demux_decode/packet_remuxing_audio.c
+++ 202008/ffmpeg_project/demux_decode/packet_remuxing_audio.c
@@ -28,4 +28,25 @@ static int get_format_from_sample_fmt(const char **fmt ,enum AVSampleFormat samp
 	return AVERROR(EINVAL);
 }
 
-/** Fill dst **/
+/** Fill dst buffer with nb_samples ,generated starting format **/
+static void fill_samples(double *dst ,int nb_samples ,int nb_channels ,int sample_rate ,double *t) {
+	int i ,j;
+	double tincr = 1.0 / sample_rate ,*dstp = dst;
+	const double c = 2 * M_PI * 440.0;
+
+	/** generate sin tone with 440Hz frequency and dumplicated channels **/
+	for (i = 0 ; i < nb_channels ; i++) {
+		*dstp =  sin(c * *t);
+		for (j = 1 ; j < nb_channels ; j++) {
+			dstp[j] = dstp[0];
+		}
+		dstp += nb_channels;
+		*t += tincr;
+	}
+}
+
+int main (int argc ,char **argv) {
+	int64_t src_ch_layout = AV_CH_LAYOUT_STEREO;
+	dst_ch_layou
+}
+
