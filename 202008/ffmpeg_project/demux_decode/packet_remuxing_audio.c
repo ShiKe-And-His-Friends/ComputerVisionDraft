1aa15a3ac2502580b21dce96e63b452babb0692a
sk95120 <765871277@qq.com>
2020-09-23 12:33:23 +0800

    1. packet audio remuxing;

--- 202008/ffmpeg_project/demux_decode/packet_remuxing_audio.c
+++ 202008/ffmpeg_project/demux_decode/packet_remuxing_audio.c
@@ -47,6 +47,107 @@ static void fill_samples(double *dst ,int nb_samples ,int nb_channels ,int sampl
 
 int main (int argc ,char **argv) {
 	int64_t src_ch_layout = AV_CH_LAYOUT_STEREO;
-	dst_ch_layou
+	dst_ch_layout = AV_CH_LAYOUT_SURROUND;
+	int src_rate = 48000 ,dst_rate = 44100;
+	uint8_t **src_data = NULL ,**dst_data = NULL;
+	int src_nb_channels = 0,dst_nb_channels = 0;
+	int src_linesize ,dst_linesize;
+	int src_nb_samples = 1024 ,dst_nb_samples ,max_dst_nb_samples;
+	enum AVSampleFormat src_sample_fmt = AV_SAMPLE_FMT_DBL ,dst_sample_fmt = AV_SAMPLE_FMT_S16;
+	const char *dst_filename = NULL;
+	FILE *dst_file;
+	int *dst_bufsize;
+	const char *fmt;
+	struct SwrContext *swr_ctx;
+	double t;
+	int ret;
+
+	if (argc != 2) {
+		fprintf(stderr ,"Usage: %s output_file \n API example program to show how to resample an audio stream with libswresample.\n This program generates a series of audio frams ,resample them to a specified ouput foramt and rate and saves them to an output file named output_file.\n" ,argv[0]);
+		exit(1);
+	}
+	/** create resampler context **/
+	swr_ctx = swr_alloc();
+	if (!swr_ctx) {
+		fprintf(stderr ,"Could not allocate resampler contxt.\n");
+		ret = AVERROR(ENOMEM):
+		goto end;
+	}
+	/** set options **/
+	av_opt_set_int(swr_ctx ,"in_channel_layout" ,src_ch_layout ,0);
+	av_opt_set_int(swr_ctx ,"in_sample_rate" ,src_rate ,0);
+	av_opt_set_sample_fmt(swr_ctx ,"in_sample_fmt" ,src_sample_fmt ,0);
+	av_opt_set_int(swr_ctx ,"out_channel_layout" ,dst_ch_layout ,0);
+	av_opt_set_int(swr_ctx ,"out_sample_rate" ,dst_rate ,0);
+	av_opt_set_resample_fmt(swr_ctx ,"out_sample_fmt" ,dst_sample_fmt ,0);
+	/** initialize the resampling context **/
+	if ((ret = swr_init(swr_ctx)) < 0) {
+		fprintf(stderr ,"Failed to initialize the resampling context.\n");
+		goto end;
+	}
+	/** allocate source and destination samples buffers **/
+	src_nb_channels = av_get_channel_layout_nb_channels(src_ch_layout);
+	ret = av_sample_alloc_array_and_samples(&src_data ,&src_linesize ,src-nb_channels ,src_nb_samples ,src_sample_fmt ,0);
+	if (ret < 0) {
+		fprintf(stderr ,"Could not allocate source samples.\n");
+		goto end;
+	}
+
+	/** compute the number of converted samples: buffering is avoided ensuring that the output buffer will contaion at leasr all the converted input samples. **/
+	max_dst_nb_samples = dst_nb_samples = av_rescale_rnd(src_nb_samples ,dst_rate ,src_rate ,AV_ROUND_UP);
+	/** buffer is going to be directly written to a rawaudio file ,no alignment **/
+	dst_nb_channels = av_get_channel_layout_nb_channels(dst_ch_layout);
+	ret = av_samples_alloc_array_and_samples(&dst_data ,&dst_linesize ,dst_nb_channels ,dst_nb_samples ,dst_sample_fmt ,0);
+	if (ret < 0) {
+		fprintf(stderr ,"Could not allocate destination samples.\n");
+		goto end;
+	}
+
+	t = 0;
+	do {
+		/** gengerate synthetic audio **/
+		fill_samples((double *)src_data[0] ,src_nb_samples ,src_nb_channels ,src_rate ,&t);
+		/** compute destination number of samples **/
+		dst_nb_samples = av_rescale_rnd(swr_get_delay(swr_ctx ,src_rate) + src_nb_samples ,dst_rate ,src_rate ,AV_ROUND_UP);
+		if (dst_mb_samples > max_dst_nb_samples) {
+			av_freep(&dst_data[0]);
+			ret = av_samples_alloc(dst_data ,&dst_linesize ,dst_nb_channels ,dst_nb_samples ,dst_sample_fmt ,1);
+			if (ret < 0) {
+				break;
+			}
+			max_dst_nb_samples = dst_nb_samples;
+		}
+		/** conert to destination format**/
+		ret = swr_convert(swr_ctx ,dst_data ,dst_nb_samples ,(const uint8_t **)src_data ,src_nb_samples);
+		if (ret < 0) {
+			fprintf(stderr ,"Error while converting\n");
+			goto end;
+		}
+		dst_bufsize = av_samples_get_buffer_size(&dst_linesize ,dst_nb_channels ,ret ,dst_sample_fmt ,1);
+		if (dst_bufsize < 0) {
+			fprintf(stderr ,"Could not get sample buffer size.\n")
+			goto end;
+		}
+		printf("t:%f in:%d out:%d \n" ,t ,src_nb_samples ,ret);
+		fwrite(dst_data[0] ,1 ,dst_bufsize ,dst_file);
+	} while (t < 10);
+
+	if ((ret = get_format_from_sample_fmt(&fmt ,dst_sample_fmt)) < 0) {
+		goto end;
+	}
+	fprintf(stderr ,"Resampling succeeded. Play the output file with the command:\n ffplay -f %s -channel_layout %"PRId64" -channels %d -ar %d %s\n " ,fmt ,dst_ch_layout ,dst_nb_channels ,dst_rate ,dst_filename);
+
+end:
+	fclose(dst_file);
+	if (src_data) {
+		av_freep(&src_data[0]);
+	}
+	av_freep(&src_data);
+	if (dst_data) {
+		av_freep(&dst_data[0]);
+	}
+	av_freep(&dst_data);
+	swr_free(&swr_ctx);
+	return ret < 0;
 }
 
