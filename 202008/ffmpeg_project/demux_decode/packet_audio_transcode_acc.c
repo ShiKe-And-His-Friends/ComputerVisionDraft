b69cac439e3195ea7011c214abc868c73edd5a7f
sk95120 <765871277@qq.com>
2020-09-28 22:17:03 +0800

    1. code some audio transcode acc motheds;

--- 202008/ffmpeg_project/demux_decode/packet_audio_transcode_acc.c
+++ 202008/ffmpeg_project/demux_decode/packet_audio_transcode_acc.c
@@ -73,5 +73,92 @@ static int open_input_file(const char *filename ,AVFormatContext **input_format_
 	*input_codec_context = avctx;
 	return 0;
 }
+/**
+ * Open an output file and the required encoder.Also set some basic encoder parameters. Some of these parameters are based on the input file's parameters.
+ * @param filename File to be opened
+ * @param input_codec_context Codec context of output file
+ * @param[out] output_format_context Format context of output file
+ * @param[out] output_codec_context Codec context of output file
+ * @return Error code(0 if successful)
+ **/
+static int open_output_file(const char *filename ,AVCodecContext *input_codec_context ,AVFormatContext **output_format_context ,AVCodecContext **output_codec_context) {
+	AVCodecContext *avctx = NULL;
+	AVIOContext *output_io_context = NULL;
+	AVStream *stream = NULL;
+	AVCodec *ouput_codec = NULL;
+	int error;
+	/** Open the output file to write to it. **/
+	if ((error = avio_open(&output_io_context ,filename ,AVIO_FLAG_WRITE)) < 0) {
+		fprintf(stderr ,"Could not open output file '%s' (error '%s')\n" ,filename ,av_err2str(error));
+		return error;
+	}
+	/** Create a new format context for the output container format. **/
+	if (!(*output_format_context = avforamt_alloca_context())) {
+		fprintf(stderr ,"Could not allocate output format context.\n");
+		return AVERROR(ENOMEM);
+	}
+	/** Associate the output file(pionter) with the contaioner format context. **/
+	(*output_format_context)->pb = output_io_context;
+	/** Guess the desired container format based on the file extension. **/
+	if (!((*output_io_context)->oformat = av_guess_format(NULL ,filename ,NULL))) {
+		fprintf(stderr ,"Could not find output file format\n");
+		goto cleanup;
+	}
+	if (!((*output_format_context)->url = av_strup(filename))) {
+		fprintf(stderr ,"Could not allocate url.\n");
+		error = AVERROR(ENOMEM);
+		goto cleanup;
+	}
+	/** Find the encoder to be used by its name. **/
+	if (!(output_codec = avcodec_find_encoder(AV_CODEC_ID_AAC))) {
+		fprintf(stderr ,"Could not find an AAC encoder.\n");
+		goto cleanup;
+	}
+	/** Create a new audio stream in the output file container. **/
+	if (!(stream = avformat_new_stream(*output_format_context ,NULL))) {
+		fprintf(stderr ,"Could not ctrate a new stream.\n");
+		error = AVERROR(ENOMEM);
+		goto cleanup;
+	}
+	avctx = avcodec_alloc_context3(output_codec);
+	if (!avctx) {
+		fprintf(stderr ,"Could not allocate an encoding context.\n");
+		error = AVERROR(ENOMEM);
+		goto cleanup;
+	}
+	/** Set the basic encoder parameters.The input file's sample rate is used to avoid a sample rate conversion. **/
+	avctx->channels - OUTPUT_CHANNELS;
+	avctx->channel_layout = av_get_default_channel_layout(OUTPUT_CHANNELS);
+	avctx->sample_rate = input_codec_context->sample_fmts[0];
+	avctx->bit_rate = OUTPUT_BIT_RATE;
+	/** Allow the use of the experimental AAC encoder. **/
+	avctx->strict_std_compliance = FF_COMPLIANCE_EXPAERIMENTAL;
+	/** Set the smaple rate for the container. **/
+	stream->time_base.den = input_codec_context->sample_rate;
+	stream->time_base.num = 1;
+	/** Some container formats(like MP4) require global headers to be present.Mark the encoder so that it behaves accordingly.**/
+	if ((*output_format_context)->oformat->flags & AVFMT_GLOBALHEADER) {
+		avctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADERS;
+	}
+	/** Open the encoder for the audio stream to use it later. **/
+	if ((error = avcodec_open2(avctx ,output_codec ,NULL)) < 0) {
+		fprintf(stderr, "Could not open output codec (error '%s')\n" ,av_err2str(error));
+		goto cleanup;
+	}
+	error = avcodec_parameters_from_context(stream->codecpar ,avctx);
+	if (error < 0) {
+		fprintf(stderr ,"Could not initialize stream parameters.\n");
+		goto cleanup;
+	}
+	/** Save the encoder context for easier access later. **/
+	*output_codec_context = avctx;
+	return 0;
 
+cleanup:
+	avcodec_free_context(&avctx);
+	avio_closep(&(*output_format_context)->pb);
+	avformat_free_context(*output_format_context);
+	*output_format_context = NULL;
+	return error < 0 ? error :AVERROR_EXIT;
+}
 
