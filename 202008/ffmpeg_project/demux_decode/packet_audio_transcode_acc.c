b3ced70c558db4b19a26b8872d5cd6644f77191c
sk95120 <765871277@qq.com>
2020-10-01 21:48:59 +0800

    1. code some audio transcode acc motheds;

--- 202008/ffmpeg_project/demux_decode/packet_audio_transcode_acc.c
+++ 202008/ffmpeg_project/demux_decode/packet_audio_transcode_acc.c
@@ -631,6 +631,66 @@ int main(int argc ,char **argv) {
 		 * need to FIFO buffer to store as many frames worth of input samples
 		 * that they make up at least one frame worth of output samples.
 		 **/
+		while(av_audio_fifo_size(fifo) < output_frame_size) {
+			/** Decode one frame worth of audio samples ,convert it to the output sample format and put it into the FIFO buffer. **/
+			if (read_decode_convert_and_store(fifo ,input_format_context ,input_codec_context 
+			,output_codec_context ,resample_context ,&finished)) {
+				goto cleanup;
+			}
+			/** If we are at the end of the input file,we continue encodeing to the output file. **/
+			if (finished) {
+				break;
+			}
+		}
+		/** If we have enough samples for the encoder ,we encode them.
+		 * At the end of the file ,we pass the remaining samples to the encoder. **/
+		while (av_audio_fifo_size(fifo) >= output_frame_size || (finished && av_audio_fifo_size(fifo) > 0)) {
+			/** Take one frame worth of audio samples from the FIFO buffer,
+			 * encode it and write it to the output file.
+			 **/
+			if (load_encode_and_write(fifo ,output_format_context ,output_codec_context)) {
+				goto cleanup;
+			}
+		}
+		/**
+		 * If we are at the end of the input file and have encoded
+		 * all remaining samples ,we can exit this loop and finish.
+		 **/
+		if (finish) {
+			int data_written;
+			/** Flush the encoder as it may have delayed frames. **/
+			do {
+				if (encode_audio_frame(NULL ,output_format_context ,output_codec_context ,&data_written)) {
+					goto cleanup;	
+				}
+			} while (data_written);
+			break;
+		}
 	}
+	/** Write the trailer of the output file container. **/
+	if (write_output_file_trailer(output_format_context)) {
+		goto cleanup;
+	}
+	ret = 0;
+
+cleanup:
+	if (fifo) {
+		av_audio_fifo_free(fifo);
+	}
+	swr_free(&resample_context);
+	if (output_codec_context) {
+		avcodec_free_context(&output_codec_context);
+	}
+	if (output_format_context) {
+		avio_closep(&output_format_context->pb);
+		avformat_free_context(output_format_context);
+	} 
+	if (input_codec_context) {
+		avcodec_free_context(&input_codec_context);
+	}
+	if (input_format_context) {
+		avformat_close_input(&input_format_context);
+	}
+	return ret;
 }
 
