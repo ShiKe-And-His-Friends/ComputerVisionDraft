462434c495afdc962486683277a8bc6d8b843c47
sk95120 <765871277@qq.com>
2020-10-02 22:08:36 +0800

    1. vappi motheds 's file name;

---
+++ 202008/ffmpeg_project/demux_decode/packet_vaapi_encode.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <libavcodec/avcodec.h>
+#include <libavutil/pixdesc.h>
+#include <libavutil/hwcontext.h>
+
+static int width,height;
+static AVBufferRef *hw_device_ctx = NULL;
+
+static int set_hwframe_ctx (AVCodecContext *ctx ,AVBufferRef *hw_device_ctx) {
+	AVBufferRef *hw_frames_ref;
+	AVHWFramesContext *frames_ctx = NULL;
+	int err = 0;
+	if (!(hw_frames_ref = avhwframe_ctx_alloc(hw_device_ctx))) {
+		fprintf(stderr, "Failed to create VAAPI frame context.\n");
+		return -1;
+	}
+	frames_ctx = (AVHWFramesContext *)(hw_frames_ref->data);
+	frames_ctx->format = AV_PIX_FMT_VAAPI;
+	frames_ctx->sw_format = AV_PIX_FMT_NV12;
+	frames_ctx->width = width;
+	frames_ctx->height = height;
+	frames_ctx->initial_pool_size = 20;
+	if ((err = av_hwframe_ctx_init(hw_frames_ref)) < 0) {
+		fprintf(stderr ,"Failed to initialize VAAPI frame context. Error code: %s\n" ,av_err2str(err));
+		av_buffer_unref(&hw_frames_ref);
+		return err;
+	}
+	ctx->hw_frames_ctx = av_buffer_ref(hw_frames_ref);
+	if (!ctx->hw_frames_ctx) {
+		err = AVERROR(ENOMEM);
+	}
+	av_buffer_unref(&hw_frames_ref);
+	return err;
+}
+
+static int encode_write(AVCodecContext *avctx ,AVFrame *frame ,FILE *fout) {
+	int ret = 0;
+	AVPacket enc_pkt;
+	av_init_packet(&enc_pkt);
+	enc_pkt.data = NULL;
+	enc_pkt.size = 0;
+	if ((ret = avcodec_send_frame(avctx ,frame)) < 0) {
+		fprintf(stderr ,"Error code: %s \n" ,av_err2str(ret));
+		goto end;
+	}
+	while (1) {
+		ret = avcodec_receive_packet(avctx ,&enc_pkt);
+		if (ret) {
+			break;
+		}
+		enc_pkt.stream_index = 0;
+		ret = fwrite(enc_pkt.data ,enc_pkt.size ,1 ,fout);
+		av_packet_unref(&enc_pkt);
+	}
+
+end:
+	ret = ((ret == AVERROR(EAGAIN) ? 0 : -1));
+	return ret;
+}
+
+int main (int argc ,char *argv[]) {
+	int size ,err;
+	FILE *fin = NULL ,*fout = NULL;
+	AVFrame *sw_frame = NULL ,*hw_frame = NULL;
+	AVCodecContext *avctx = NULL;
+	AVCodec *codec = NULL;
+	const char *enc_name ="h264_vappi";
+
+	if (argc < 5) {
+		fprintf(stderr ,"Usage: %s <width> <height> <input file> <output file>\n" ,argv[0]);
+		return -1;
+	}
+	width = atoi(argv[1]);
+	height = atoi(rgv[2]);
+	size = width * height;
+	if (!(fin =fopen(argv[3] ,"r"))) {
+		fprintf(stderr, "Fail to open input : %s \n" ,strerror(errno));
+		return -1;
+	}
+	if (!(fout = fopen(argv[4] ,"w+b"))) {
+		fprintf(stderr ,"Fail to open output file: %s \n" ,strerror(errno));
+		err = -1;
+		goto close;
+	}
+	err = av_hwdevice_ctx_create(&hw_device_ctx ,AV_HWDEVICE_TYPE_VAAPI ,NULL ,NULL ,0);
+	if (err < 0) {
+		fprintf(stderr ,"Failed to create a VAAPI device. Error code : %s \n" ,av_err2str(err));
+		goto close;
+	}
+	if (!(codec = avcodec_find_encoder_by_name(enc_name))) {
+		fprintf(stderr ,"Could not find encoder.\n");
+		err = -1;
+		goto close;
+	}
+
+	if (!(avctx = avcodec_alloc_context3(codec))) {
+		err =AVERROR(ENOMEM);
+		goto close;
+	}
+	avctx->width = width;
+	avctx->height = height;
+	avctx->time_base = (AVRational){1 ,25};
+	avctx->framerate = (AVRational){25 ,1};
+	avctx->sample_aspect_ratio = (AVRational){1 ,1};
+	avctx->pix_fmt = AV_PIX_FMT_VAAPI;
+	/** set hw_frames_ctx for encoder's AVCodecContext **/
+	if ((err = set_hwframe_ctx(avctx ,hw_device_ctx)) < 0) {
+		fprintf(stderr ,"Failed to set hwframe context.\n");
+		goto close;
+	}
+	if ((err = avcodec_open2(avctx ,codec ,NULL) < 0)) {
+		fprintf(stderr ,"Cannot open video encoder codec. Error code:%s \n" ,av_err2str(err));
+		goto close;
+	}
+}
