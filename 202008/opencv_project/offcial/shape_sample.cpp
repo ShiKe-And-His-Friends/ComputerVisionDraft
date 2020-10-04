98f2de1bf31f2833ebf5514ab3e34d313622d97e
sk95120 <765871277@qq.com>
2020-10-04 19:24:13 +0800

    1. modify project directory;

---
+++ 202008/opencv_project/offcial/shape_sample.cpp
@@ -0,0 +1,19 @@
+/**
+ * An example using shape distance algorithm
+ * Shape context demo for shape mathing
+ **/
+#include "opencv2/shape.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <opencv2/core/utility.hpp>
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+int main (int argc ,char** argv) {
+
+	return 0;
+}
