d57a5fb86189aa8836d3e778b9179c40e1ee49ae
sk95120 <765871277@qq.com>
2020-10-05 18:23:44 +0800

    fix bug: 1. camera tracker compile failure; 2. CMake Chain error; 1. modify project directory;

--- 202008/opencv_project/offcial/camshiftdemo.cpp
+++ 202008/opencv_project/offcial/camshiftdemo.cpp
@@ -5,7 +5,7 @@
 #include "opencv2/video/tracking.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
-#include "opencv2/heighgui.hpp"
+#include "opencv2/highgui.hpp"
 
 #include <iostream>
 #include <ctype.h>
@@ -24,13 +24,13 @@ Rect selection;
 int vmin = 10 ,vmax = 256 ,smin = 30;
 
 // User draws box around object to track. This triggers CAMShift to start tracking
-static void onMouse (int event ,int x ,in y ,int, void*) {
+static void onMouse (int event ,int x ,int y ,int, void*) {
 	if (selectObject) {
 		selection.x = MIN(x ,origin.x);
 		selection.y = MIN(y ,origin.y);
 		selection.width = std::abs(x - origin.x);
 		selection.height = std::abs(y - origin.y);
-		selection &= Rect(0 ,0 ,image.cols ,imahe.rows);
+		selection &= Rect(0 ,0 ,image.cols ,image.rows);
 	}
 	switch (event) {
 		case EVENT_LBUTTONDOWN:
@@ -41,8 +41,7 @@ static void onMouse (int event ,int x ,in y ,int, void*) {
 		case EVENT_LBUTTONUP:
 			selectObject = false;
 			if (selection.width > 0 && selection.height > 0) {
-				trackObject = -1; //Set up CAMShift properties
-				in main() loop;
+				trackObject = -1; //Set up CAMShift properties in main() loop;
 			}
 			break;
 	}
@@ -70,7 +69,7 @@ const char* keys = {
 	"{help h | | show help message}{@camera_number| 0 | camera number}"
 };
 
-int main (int argc ,cont char** argv) {
+int main (int argc ,const char** argv) {
 	VideoCapture cap;
 	Rect trackWindow;
 	int hsize = 16;
@@ -91,12 +90,12 @@ int main (int argc ,cont char** argv) {
 		return -1;
 	}
 	cout <<hot_keys;
-	nameWindow("Histogram" ,0);
-	nameWindow("CamShift Demo" ,0);
+	namedWindow("Histogram" ,0);
+	namedWindow("CamShift Demo" ,0);
 	setMouseCallback("CamShift Demo" ,onMouse ,0);
-	createTracker("Vmin" ,"CamShift Demo" ,&vmin ,256 ,0);
-	createTracker("Vmax" ,"CamShift Demo" ,&vmax ,256 ,0);
-	createTracker("Smin" ,"CamShift Demo" ,&smin ,256 ,0);
+	createTrackbar("Vmin" ,"CamShift Demo" ,&vmin ,256 ,0);
+	createTrackbar("Vmax" ,"CamShift Demo" ,&vmax ,256 ,0);
+	createTrackbar("Smin" ,"CamShift Demo" ,&smin ,256 ,0);
 
 	Mat frame ,hsv ,hue ,mask ,hist ,histimg = 
 	Mat::zeros(200 ,320 ,CV_8UC3) ,backproj;
@@ -108,7 +107,7 @@ int main (int argc ,cont char** argv) {
 				break;
 			}
 		}
-		frame.copyTo(iamge);
+		frame.copyTo(image);
 		if (!paused) {
 			cvtColor(image ,hsv ,COLOR_BGR2HSV);
 			if (trackObject) {
@@ -121,18 +120,18 @@ int main (int argc ,cont char** argv) {
 				if (trackObject < 0) {
 					// Object has been selected by user ,set up CAMShift search properties once 
 					Mat roi(hue ,selection) ,maskroi(mask ,selection);
-					calchist(&roi ,1 ,0 ,maskroi ,hist ,1 ,&hsize ,&phranges);
+					calcHist(&roi ,1 ,0 ,maskroi ,hist ,1 ,&hsize ,&phranges);
 					normalize(hist ,hist ,0 ,255 ,NORM_MINMAX);
 					trackWindow = selection;
 					// Don't set up again ,unless user selects new ROI
 					trackObject = 1;
 					histimg = Scalar::all(0);
-					int binW = histimg,cols /hsize;
+					int binW = histimg.cols /hsize;
 					Mat buf(1 ,hsize ,CV_8UC3);
 					for (int i = 0 ; i < hsize ; i++) {
-						buf.ar<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180./hsize) ,255 ,255);
+						buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180./hsize) ,255 ,255);
 						cvtColor(buf ,buf ,COLOR_HSV2BGR);
-						for (int i = 0 ; i < hszie ; i++) {
+						for (int i = 0 ; i < hsize ; i++) {
 							int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows/255);
 							rectangle(histimg ,Point(i*binW ,histimg.rows), Point((i+1)*binW ,histimg.rows - val) ,Scalar(buf.at<Vec3b>(i)) ,-1 ,8);
 						}
@@ -142,7 +141,7 @@ int main (int argc ,cont char** argv) {
 					backproj &= mask;
 					RotatedRect trackBox = CamShift(backproj ,trackWindow ,TermCriteria(TermCriteria::EPS | TermCriteria::COUNT ,10 ,1));
 					if (trackWindow.area() <= 1) {
-						int clos = backproj.cols ,rows = backproj.rows ,r = (MIN(cols ,rows) + 5) / 6;
+						int cols = backproj.cols ,rows = backproj.rows ,r = (MIN(cols ,rows) + 5) / 6;
 						trackWindow = Rect(trackWindow.x - r ,trackWindow.y - r ,trackWindow.x + r ,trackWindow.y + r) & Rect(0 ,0 ,cols ,rows);
 					}
 					if (backprojMode) {
@@ -177,7 +176,7 @@ int main (int argc ,cont char** argv) {
 			case 'h':
 				showHist = !showHist;
 				if (!showHist) {
-					desroyWindow("Histogram");
+					destroyWindow("Histogram");
 				} else {
 					namedWindow("Histogram" ,1);
 				}
