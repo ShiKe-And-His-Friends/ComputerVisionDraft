8af66323d6fc275c0d38f840e42ebdb4e0a7e9b2
sk95120 <765871277@qq.com>
2020-10-06 08:20:41 +0800

    1. contours methods;

---
+++ 202008/opencv_project/offcial/contours2.cpp
@@ -0,0 +1,15 @@
+/**
+ * An example program illustrates the use of cv::findContours
+ * and cv::drawContours
+ * */
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include <math.h>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc ,char** argv) {
+	return 0;
+}
