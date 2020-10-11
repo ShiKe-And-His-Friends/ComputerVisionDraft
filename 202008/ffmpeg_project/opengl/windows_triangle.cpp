b46ebcfafe0d7825100c8816bfe493bf22433c43
sk95120 <765871277@qq.com>
2020-10-11 09:48:47 +0800

    fix bug:1. shader runtime error;

--- 202008/ffmpeg_project/opengl/windows_triangle.cpp
+++ 202008/ffmpeg_project/opengl/windows_triangle.cpp
@@ -11,16 +11,16 @@ const unsigned int SCR_HEIGHT = 600;
 
 using namespace std;
 
-const char *vertexShaderSource = "#version 331 core\n"
+const char *vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
 	"void main () \n"
 	"{ \n"
 	"	gl_Position = vec4(aPos.x ,aPos.y ,aPos.z ,1.0);\n"
-	"}\0";
-const char* fragmentShaderSource = "#version 331 core\n"
+	"}\n\0";
+const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
 	"void main () \n"
-	"( \n"
+	"{ \n"
 	"	FragColor = vec4(1.0f ,0.5f ,0.2f ,1.0f);\n"
 	"}\n\0";
 
