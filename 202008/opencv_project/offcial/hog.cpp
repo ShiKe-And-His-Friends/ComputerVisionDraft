4089d66dcbfb782cc41d4f9d4e93bc67cfadd759
sk95120 <765871277@qq.com>
2020-11-01 20:27:13 +0800

    1. add hog hpp

---
+++ 202008/opencv_project/offcial/hog.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <stdexcept>
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgroc.hpp>
+
+
+
