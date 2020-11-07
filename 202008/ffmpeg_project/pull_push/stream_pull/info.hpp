4842ee832ee334754149182dfedaf27d6a4d966b
sk95120 <765871277@qq.com>
2020-11-07 11:24:58 +0800

    1. pull stream rtmp stream;

---
+++ 202008/ffmpeg_project/pull_push/stream_pull/info.hpp
@@ -0,0 +1,7 @@
+#ifndef __INFO_H__  //info.h  
+#define __INFO_H__
+  
+	#include <string.h>
+	#include <stdio.h>
+  
+#endif
---
+++ 202008/ffmpeg_project/pull_push/stream_pull/server_or_local_pull_main.cpp
@@ -0,0 +1,24 @@
+/**
+ * Server or local pull stream
+ * https://blog.csdn.net/zhuweigangzwg/article/details/43734251
+ */
+ 
+#include "server_or_local_pull.hpp"
+
+int main(int argc ,char ** argv)  
+{  
+    av_register_all();  
+    avformat_network_init();  
+  
+    InitInput(INPUTURL,&icodec,&ifmt);  
+    InitOutput();  
+    printf("--------程序运行开始----------\n");  
+    //////////////////////////////////////////////////////////////////////////  
+    Es2Mux_2();  
+    //////////////////////////////////////////////////////////////////////////  
+    UintOutput();  
+    UintInput();  
+    printf("--------程序运行结束----------\n");  
+    printf("-------请按任意键退出---------\n");  
+    return getchar();  
+}
