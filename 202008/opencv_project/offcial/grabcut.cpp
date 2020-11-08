cdc0f916fef19a27b00105bd6e23a8df04fb61d8
sk95120 <765871277@qq.com>
2020-11-08 21:33:26 +0800

    1. grab cut;

--- 202008/opencv_project/offcial/grabcut.cpp
+++ 202008/opencv_project/offcial/grabcut.cpp
@@ -31,7 +31,7 @@ const Scalar RED = Scalar(0 ,255 ,255);
 const Scalar PINK = Scalar(230 ,130 ,255);
 const Scalar BLUE = Scalar(255 ,0 ,0);
 const Scalar LIGHTBLUE = Scalar(255 ,255 ,160);
-const Scalar GREEN = Scalar(0 ,255 ,0):
+const Scalar GREEN = Scalar(0 ,255 ,0);
 
 const int BGD_KEY = EVENT_FLAG_CTRLKEY;
 const int FGD_KEY = EVENT_FLAG_SHIFTKEY;
@@ -76,11 +76,11 @@ private:
 	Mat mask;
 	Mat bgdModel, fgdModel;
 
-	uchar rectState, lblsState, prLblesState;
+	uchar rectState, lblsState, prLblsState;
 	bool isInitialized;
 
 	Rect rect;
-	vector<Point> fgbPxls, bgdPxls, prFgdPxels, prBgPxls;
+	vector<Point> fgbPxls, bgdPxls, prFgdPxls, prBgdPxls;
 	int iterCount;
 };
 
@@ -91,11 +91,11 @@ void GCApplication::reset() {
 	bgdPxls.clear();
 	fgbPxls.clear();
 	prBgdPxls.clear();
-	prFgdpxls.clear();
+	prFgdPxls.clear();
 
 	isInitialized = false;
 	rectState = NOT_SET;
-	bkbsState = NOT_SET;
+	lblsState = NOT_SET;
 	prLblsState = NOT_SET;
 	iterCount = 0;
 }
@@ -106,7 +106,7 @@ void GCApplication::setLblsInMask(int flags ,Point p ,bool isPr) {
 	uchar bvalue ,fvalue;
 	if (!isPr) {
 		bpxls = &bgdPxls;
-		fpxls = &fgdPxls;
+		fpxls = &fgbPxls;
 		bvalue = GC_BGD;
 		fvalue = GC_PR_FGD;
 	}
@@ -124,45 +124,51 @@ void GCApplication::mouseClick(int event ,int x ,int y ,int flags ,void*) {
 	//TODO add bad args check
 	switch (event) {
 		case EVENT_LBUTTONDOWN:
-			bool isb = (flags & BGD_KEY) != 0;
-			bool isf = (flags & FGD_KEY) != 0;
-			if (rectState == NOT_SET && !isb && !isf) {
-				rectState = IN_PROCESS;
-				rect = Rect(x ,y ,1 ,1);
-			}
-			if ((isb || isf) && rectState == SET) {
-				lblsState == IN_PROCESS;
+			{
+				bool isb = (flags & BGD_KEY) != 0;
+				bool isf = (flags & FGD_KEY) != 0;
+				if (rectState == NOT_SET && !isb && !isf) {
+					rectState = IN_PROCESS;
+					rect = Rect(x ,y ,1 ,1);
+				}
+				if ((isb || isf) && rectState == SET) {
+					lblsState == IN_PROCESS;
+				}
 			}
 			break;
 			
 		case EVENT_RBUTTONDOWN:
 			// set GC_PR_BGD labels
-			bool isb = (flags & BGD_KEY) != 0;
-			bool isf = (flags & FGD_KEY) != 0;
-			if ((isb || isf) && rectState == SET) {
-				prLblesState = IN_PROCESS;
+			{
+				bool isb = (flags & BGD_KEY) != 0;
+				bool isf = (flags & FGD_KEY) != 0;
+				if ((isb || isf) && rectState == SET) {
+					prLblsState = IN_PROCESS;
+				}
 			}
 			break;
 			
 		case EVENT_LBUTTONUP:
-			if (rectState == IN_PROCESS) {
-				rect = Rect(Point(rect.x ,rect.y) ,Point(x ,y));
-				rectState = SET;
-				setRectInMask();
-				CV_Assert(bgdPxls.empty() && fgdPxls.empty() && prBgPxls.empty() && prBgPxls.empty());
-				showImage();
-			}
-			if (lblsState == IN_PROCESS) {
-				setLblsInMask(flags ,Point(x ,y) ,false);
-				lblsState = SET;
-				showImage();
+			{
+				if (rectState == IN_PROCESS) {
+					rect = Rect(Point(rect.x ,rect.y) ,Point(x ,y));
+					rectState = SET;
+					setRectInMask();
+					CV_Assert(bgdPxls.empty() && fgbPxls.empty() && prBgdPxls.empty() && prBgdPxls.empty());
+					showImage();
+				}
+				if (lblsState == IN_PROCESS) {
+					setLblsInMask(flags ,Point(x ,y) ,false);
+					lblsState = SET;
+					showImage();
+				}
 			}
 			break;
 			
 		case EVENT_RBUTTONUP:
-			if (prLblesState == IN_PROCESS) {
+			if (prLblsState == IN_PROCESS) {
 				setLblsInMask(flags ,Point(x ,y) ,true);
-				prLblesState = SET;
+				prLblsState = SET;
 				showImage();
 			}
 			break;
@@ -170,12 +176,12 @@ void GCApplication::mouseClick(int event ,int x ,int y ,int flags ,void*) {
 		case EVENT_MOUSEMOVE:
 			if (rectState == IN_PROCESS) {
 				rect = Rect(Point(rect.x ,rect.y) ,Point(x ,y));
-				CV_Assert(bgdPxls.empty() && fgdPxls.empty() && prBgdPxls.empty() && prFgdPxels.empty());
+				CV_Assert(bgdPxls.empty() && fgbPxls.empty() && prBgdPxls.empty() && prFgdPxls.empty());
 				showImage();
 			} else if (lblsState == IN_PROCESS) {
 				setLblsInMask(flags ,Point(x ,y) ,false);
 				showImage();
-			} else if (prLblesState == IN_PROCESS) {
+			} else if (prLblsState == IN_PROCESS) {
 				setLblsInMask(flags ,Point(x ,y) ,true);
 				showImage();
 			}
@@ -185,12 +191,12 @@ void GCApplication::mouseClick(int event ,int x ,int y ,int flags ,void*) {
 
 int GCApplication::nextIter() {
 	if (isInitialized) {
-		grabCut(*image ,mask ,recr ,bgdModel ,fgdModel ,1);
+		grabCut(*image ,mask ,rect ,bgdModel ,fgdModel ,1);
 	} else {
 		if (rectState != SET) {
 			return iterCount;
 		}
-		if (lblsState == SET || prLblesState == SET) {
+		if (lblsState == SET || prLblsState == SET) {
 			grabCut(*image ,mask ,rect ,bgdModel ,fgdModel ,1 ,GC_INIT_WITH_MASK);
 		} else {
 			grabCut(*image ,mask ,rect ,bgdModel ,fgdModel ,1 ,GC_INIT_WITH_RECT);
@@ -199,9 +205,9 @@ int GCApplication::nextIter() {
 	}
 	iterCount ++;
 	bgdPxls.clear();
-	fgdPxls.clear();
-	prBgPxls.clear();
-	prFgdPxels.clear();
+	fgbPxls.clear();
+	prBgdPxls.clear();
+	prFgdPxls.clear();
 	
 	return iterCount;
 }
@@ -235,7 +241,7 @@ int main(int argc ,char** argv) {
 	for (;;) {
 		char c = (char)waitKey(0);
 		switch(c) {
-			case '\xlb':
+			case '\x1b':
 				cout << "Exiting..." << endl;
 				goto exit_main;
 				
