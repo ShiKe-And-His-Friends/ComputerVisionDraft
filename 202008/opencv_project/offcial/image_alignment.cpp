3db545afe3a12983b600b578d639aa4d3b6a0492
sk95120 <765871277@qq.com>
2020-11-16 09:41:55 +0800

    1. iamge aligment;

--- 202008/opencv_project/offcial/image_alignment.cpp
+++ 202008/opencv_project/offcial/image_alignment.cpp
@@ -1,3 +1,34 @@
+/*
+ * This sample demonstrates the use of the function findTransfromECC that implements the image alignment ECC algorithem
+ * 
+ * The demo loads an image (default to fruits.jpg) and it artificaially creates a template image based on the given motion type.
+ * When to image are given ,the first image is the input image and the second on defines the template image.
+ * In the latter case ,you can also parse the warp's initialization.
+ * Input and output warp files consist of raw warp (transform) elements
+ * Authors: G.Evangelidis ,INRIA ,Grenoble ,France
+ *          M.Asbach ,Fraunhofer IAIS ,St. Augusion ,Germany
+ */
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core/utility.hpp>
+
+#include <stdio.h>
+#include <string>
+#include <time.h>
+#include <iostream>
+#include <fstream>
+
+using namespace cv;
+using namespace std;
+
+static void help(void);
+static int readWrap(string iFilename ,mat& warp ,int motionType);
+static int saveWarp(string fileName ,const Mat& warp ,int motionType);
+static void draw_warped_roi(Mat& image ,const int width ,const int height ,Mat& W);
+
 #define HOMO_VECTOR(H ,x ,y)\
 	H.at<float>(0 ,0) = (float)(x);\
 	H.at<float>(1 ,0) = (float)(y);\
@@ -5,3 +36,10 @@
 #define GET_HOMO_VALUES(X ,x ,y)\
 	(x) = static_cast<float> (X.at<float>(0 ,0) / X.at<float>(2 ,0));\
 	(y) = static_cast<float> (X.at<float>(1 ,0) / X.at<float>(2 ,0));
+	
+const std::string keys = 
+	"{@inputImage		| fruits.jpg	| input image filename}"
+	"{@templateImage	| 				| template image filename (optionnal)}"
+	"{@inputWarp		|				| input warp (maxtrix) filename (optional)}"
+	"{n numofIte		| 50 			| ECC's iterations}"
+	"{}"
