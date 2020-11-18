8f777fca00bf857a57e43cb393430c1e2107f58d
sk95120 <765871277@qq.com>
2020-11-18 23:41:11 +0800

    1. intersect example;

---
+++ 202008/opencv_project/offcial/intersectExample.cpp
@@ -0,0 +1,17 @@
+/**
+ * Authout: Steve Nicholson
+ * A program that illukstrates intersectConvexConvex in various scenarios
+ * */
+
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+
+using namespace cv;
+using namespace std;
+
+// craete a vector of points describing a rectangle with the given corners
+static vector<Point> makeRectangle(Point topLeft ,Point bottomRight) {
+	vector<Point> rectangle;
+	rectangle.push_back(topLeft);
+}
+
