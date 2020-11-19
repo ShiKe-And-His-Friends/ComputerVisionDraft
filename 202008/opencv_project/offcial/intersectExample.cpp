74b24a5d65998f8508817a7caed81c74d86a77c4
sk95120 <765871277@qq.com>
2020-11-19 23:50:00 +0800

    1. instersect image;

--- 202008/opencv_project/offcial/intersectExample.cpp
+++ 202008/opencv_project/offcial/intersectExample.cpp
@@ -13,5 +13,17 @@ using namespace std;
 static vector<Point> makeRectangle(Point topLeft ,Point bottomRight) {
 	vector<Point> rectangle;
 	rectangle.push_back(topLeft);
+	rectangle.push_back(Point(bottomRight.x ,topLeft.y));
+	rectangle.push_back(bottomRight);
+	rectangle.push_back(Point(topLeft.x ,bottomRight.y));
+	return rectangle;
+}
+
+static vector<Point> makeTriangle(Point point1 ,Point point2 ,Point point3) {
+	vector<Point> triangle;
+	triangle.push_back(point1);
+	triangle.push_back(point2);
+	triangle.push_back(point3);
+	return triangle;
 }
 
