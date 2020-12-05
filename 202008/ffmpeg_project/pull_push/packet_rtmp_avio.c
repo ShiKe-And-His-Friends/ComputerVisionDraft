da71b336e47223203c52c90502876cf8fecb5e31
sk95120 <765871277@qq.com>
2020-12-05 10:09:29 +0800

    1. initialize the rtmp push that using remuxing code;

--- 202008/ffmpeg_project/pull_push/packet_rtmp_avio.c
+++ 202008/ffmpeg_project/pull_push/packet_rtmp_avio.c
@@ -4,14 +4,14 @@
 int main (int argc ,char **argv) {
 	int ret;
 	const char *input_file_name ,*output_file_name;
-	AVFormatContext **ifmt_ctx;
+	AVFormatContext *ifmt_ctx;
 	if (argc < 3) {
 		printf("Usage: %s intput output\n" ,argv[0]);
 		return 1;
 	}
 	input_file_name = argv[1];
 	output_file_name = argv[2];
-	if ((ret = avformat_open_input(ifmt_ctx ,input_file_name ,0 ,0)) < 0) {
+	if ((ret = avformat_open_input(&ifmt_ctx ,input_file_name ,0 ,0)) < 0) {
 		fprintf(stderr ,"Could not open input file %s," ,input_file_name);
 		goto end;
 	}
