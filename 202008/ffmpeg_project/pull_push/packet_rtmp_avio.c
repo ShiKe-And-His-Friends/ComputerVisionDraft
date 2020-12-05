c7d74f2e482c466d1a6a33f2a0eb79f040433481
sk95120 <765871277@qq.com>
2020-12-05 10:08:07 +0800

    1. debug initialize rtmp push;

--- 202008/ffmpeg_project/pull_push/packet_rtmp_avio.c
+++ 202008/ffmpeg_project/pull_push/packet_rtmp_avio.c
@@ -4,7 +4,7 @@
 int main (int argc ,char **argv) {
 	int ret;
 	const char *input_file_name ,*output_file_name;
-	AVFormatContext *ifmt_ctx;
+	AVFormatContext **ifmt_ctx;
 	if (argc < 3) {
 		printf("Usage: %s intput output\n" ,argv[0]);
 		return 1;
@@ -13,10 +13,11 @@ int main (int argc ,char **argv) {
 	output_file_name = argv[2];
 	if ((ret = avformat_open_input(ifmt_ctx ,input_file_name ,0 ,0)) < 0) {
 		fprintf(stderr ,"Could not open input file %s," ,input_file_name);
+		goto end;
 	}
 	printf("shikeDebug ...");
 	
-end;
+end:
 	avformat_close_input(&ifmt_ctx);
 	
 }
