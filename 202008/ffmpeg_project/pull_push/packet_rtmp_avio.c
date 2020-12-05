2e40b7bc070e5b5d66b6f2e6bbb9d63440051446
sk95120 <765871277@qq.com>
2020-12-05 10:30:10 +0800

    1. initialize the rtmp push that using remuxing code;

--- 202008/ffmpeg_project/pull_push/packet_rtmp_avio.c
+++ 202008/ffmpeg_project/pull_push/packet_rtmp_avio.c
@@ -4,7 +4,7 @@
 int main (int argc ,char **argv) {
 	int ret;
 	const char *input_file_name ,*output_file_name;
-	AVFormatContext *ifmt_ctx;
+	AVFormatContext *ifmt_ctx = NULL;
 	if (argc < 3) {
 		printf("Usage: %s intput output\n" ,argv[0]);
 		return 1;
@@ -12,10 +12,15 @@ int main (int argc ,char **argv) {
 	input_file_name = argv[1];
 	output_file_name = argv[2];
 	if ((ret = avformat_open_input(&ifmt_ctx ,input_file_name ,0 ,0)) < 0) {
-		fprintf(stderr ,"Could not open input file %s," ,input_file_name);
+		fprintf(stderr ,"Could not open input file %s" ,input_file_name);
 		goto end;
 	}
-	printf("shikeDebug ...");
+	if ((ret = avformat_find_stream_info(ifmt_ctx ,0)) < 0) {
+		fprintf(stderr ,"Failed to retrieve input stream information");
+		goto end;
+	}
+	av_dump_format(ifmt_ctx ,0 ,input_file_name ,0);
+	printf("shikeDebug... ret = %d \n" ,ret);
 	
 end:
 	avformat_close_input(&ifmt_ctx);
