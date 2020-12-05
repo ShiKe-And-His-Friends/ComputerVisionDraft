111fefc8fc2c7e6cb8b7df890b27a10cf62f0f5d
sk95120 <765871277@qq.com>
2020-12-05 12:01:26 +0800

    1. initialize the rtmp push that using remuxing code;

--- 202008/ffmpeg_project/pull_push/packet_rtmp_avio.c
+++ 202008/ffmpeg_project/pull_push/packet_rtmp_avio.c
@@ -4,8 +4,8 @@
 static void log_packet(const AVFormatContext *fmt_ctx ,const AVPacket *pkt ,const char *tag) {
 	AVRational *time_base = &fmt_ctx->streams[pkt->stream_index]->time_base;
 	printf("%s: pts:%s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d \n" 
-		,tag ,av_ts2str(pkt->pts) ,av_ts2timestr(pkt->pts ,time_base) ,av_ts2str(pkt->dts) ,av_ts2str(pkt->dts ,time_base)
-		,av_ts2str(pkt->duration) ,av_ts2str(pkt->duration ,time_base) ,pkt->stream_index);
+		,tag ,av_ts2str(pkt->pts) ,av_ts2timestr(pkt->pts ,time_base) ,av_ts2str(pkt->dts) ,av_ts2timestr(pkt->dts ,time_base)
+		,av_ts2str(pkt->duration) ,av_ts2timestr(pkt->duration ,time_base) ,pkt->stream_index);
 }
 
 int main (int argc ,char **argv) {
