473951ab509db97bacffd943875f5b19d99f4546
sk95120 <765871277@qq.com>
2021-01-01 17:29:14 +0800

    1. glfw window;

--- 202008/opengl_project/learnopengl_cn_github_io/1_2_windows_show.cpp
+++ 202008/opengl_project/learnopengl_cn_github_io/1_2_windows_show.cpp
@@ -1,5 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
 
 int main () {
 
@@ -7,6 +10,17 @@ int main () {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR ,3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR ,3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE ,GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT ,GL_TRUE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT ,GL_TRUE);
+	
+	GLFWwindow* window = glfwCreateWindow(800 ,600 ,"LearnOpenGl" ,NULL ,NULL);
+	if (window == NULL) {
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	} else {
+		cout << "Success to create GLFW window" << endl;
+	}
+	glfwMakeContextCurrent(window);
+
 	return 0;
 }
