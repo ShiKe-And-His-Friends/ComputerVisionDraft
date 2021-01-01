0d90ac26b2447fde069e8805ee8a1e20b65f7a25
sk95120 <765871277@qq.com>
2021-01-01 23:06:26 +0800

    1. add rectangle;

--- 202008/opengl_project/learnopengl_cn_github_io/1_4_rectangle.cpp
+++ 202008/opengl_project/learnopengl_cn_github_io/1_4_rectangle.cpp
@@ -84,17 +84,29 @@ int main () {
 	glDeleteShader(fragmentShader);
 	
 	float vertices[] = {
-		-0.5f ,-0.5f ,0.0f
+		0.5f ,0.5f ,0.0f
 		,0.5f ,-0.5f ,0.0f
-		,0.0f ,0.5f ,0.0f
+		,-0.5f ,-0.5f ,0.0f
+		,-0.5f ,0.5f ,0.0f
 	};
 
-	unsigned int VBO ,VAO;
+	unsigned int indices[] = {
+		0 ,1 ,3
+		,1 ,2 ,3
+	};
+
+	unsigned int VBO ,VAO ,EBO;
 	glGenVertexArrays(1 ,&VAO);
 	glGenBuffers(1 ,&VBO);
+	glGenBuffers(1 ,&EBO);
+
 	glBindVertexArray(VAO);
+
 	glBindBuffer(GL_ARRAY_BUFFER ,VBO);
 	glBufferData(GL_ARRAY_BUFFER ,sizeof(vertices) ,vertices ,GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER ,EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER ,sizeof(indices) ,indices ,GL_STATIC_DRAW);
+
 
 	glVertexAttribPointer(0 ,3 ,GL_FLOAT ,GL_FALSE ,3 * sizeof(float) ,(void*)0);
 	glEnableVertexAttribArray(0);
@@ -109,7 +121,8 @@ int main () {
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES ,0 ,3);
+		// glDrawArrays(GL_TRIANGLES ,0 ,6);
+		glDrawElements(GL_TRIANGLES ,6 ,GL_UNSIGNED_INT ,0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -117,6 +130,7 @@ int main () {
 
 	glDeleteVertexArrays(1 ,&VAO);
 	glDeleteBuffers(1 ,&VBO);
+	glDeleteBuffers(1 ,&EBO);
 	glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
