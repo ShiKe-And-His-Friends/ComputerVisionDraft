6e101da2941d92fe962d43a27112e0c342676677
sk95120 <765871277@qq.com>
2021-01-03 16:21:21 +0800

    1. base out storage code;

--- 202008/opengl_project/learnopengl_cn_github_io/1_5_shader_3_storage.cpp
+++ 202008/opengl_project/learnopengl_cn_github_io/1_5_shader_3_storage.cpp
@@ -7,26 +7,10 @@ using namespace std;
 void framebuffer_size_callback(GLFWwindow* window ,int width ,int height);
 void processInput(GLFWwindow* windows);
 
+#include "raw/shader_s_1.h"
+
 const unsigned int SRC_WIDTH = 800;
 const unsigned int SRC_HEIGHT = 600;
-
-const char* vertexShaderSource = "#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"layout (location = 1) in vec3 aColor;\n"
-	"out vec3 ourColor;\n"
-	"void main() \n"
-	"{\n"
-	"  gl_Position = vec4(aPos ,1.0);\n"
-	"  ourColor = aColor;\n"
-	"}\0";
-
-const char* fragmentShaderSource = "#version 330 core\n"
-	"out vec4 FragColor;\n"
-	"in vec3 ourColor;\n"
-	"void main()\n"
-	"{\n"
-	"  FragColor = vec4(ourColor ,1.0f);\n"
-	"}\n\0";
 
 int main () {
 
@@ -57,36 +41,8 @@ int main () {
 		cout << "Success initialize GLAD" << endl;	
 	}
 
-	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader ,1 ,&vertexShaderSource ,NULL);
-	glCompileShader(vertexShader);
-	int success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader ,GL_COMPILE_STATUS ,&success);
-	if (!success) {
-		glGetShaderInfoLog(vertexShader ,512 ,NULL ,infoLog);
-		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
-	}
-	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader ,1 ,&fragmentShaderSource ,NULL);
-	glCompileShader(fragmentShader);
-	glGetShaderiv(fragmentShader ,GL_COMPILE_STATUS ,&success);
-	if (!success) {
-		glGetShaderInfoLog(fragmentShader ,512 ,NULL ,infoLog);
-		cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << endl;
-	}
-	int shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram ,vertexShader);
-	glAttachShader(shaderProgram ,fragmentShader);
-	glLinkProgram(shaderProgram);
-	glGetProgramiv(shaderProgram ,GL_LINK_STATUS ,&success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram ,512 ,NULL ,infoLog);
-		cout << "ERROR::SHADER::PROGRAME::COMPILATION_FAILED\n" << infoLog << endl;
-	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-	
+	Shader outShader("3.3.shader.vs" ,"3.3.shader.fs");
+
 	float vertices[] = {
 		// position	// color
 		0.5f ,-0.5f ,0.0f ,1.0f ,0.0f ,0.0f
@@ -98,6 +54,7 @@ int main () {
 	glGenVertexArrays(1 ,&VAO);
 	glGenBuffers(1 ,&VBO);
 	glBindVertexArray(VAO);
+
 	glBindBuffer(GL_ARRAY_BUFFER ,VBO);
 	glBufferData(GL_ARRAY_BUFFER ,sizeof(vertices) ,vertices ,GL_STATIC_DRAW);
 
@@ -112,6 +69,8 @@ int main () {
 		processInput(window);
 		glClearColor(0.2f ,0.3f ,0.3f ,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		outShader.use();
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES ,0 ,3);
