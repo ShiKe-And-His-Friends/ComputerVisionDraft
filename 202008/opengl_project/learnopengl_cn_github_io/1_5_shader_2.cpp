1de2681ea79673761a7fcab3579376087bedf36d
sk95120 <765871277@qq.com>
2021-01-03 15:06:09 +0800

    1. add shader color triangle that rainbow;

--- 202008/opengl_project/learnopengl_cn_github_io/1_5_shader_2.cpp
+++ 202008/opengl_project/learnopengl_cn_github_io/1_5_shader_2.cpp
@@ -12,16 +12,20 @@ const unsigned int SRC_HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+	"out vec3 ourColor;\n"
 	"void main() \n"
 	"{\n"
-	"  gl_Position = vec4(aPos.x ,aPos.y ,aPos.z ,1.0);\n"
+	"  gl_Position = vec4(aPos ,1.0);\n"
+	"  ourColor = aColor;\n"
 	"}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
+	"in vec3 ourColor;\n"
 	"void main()\n"
 	"{\n"
-	"  FragColor = vec4(1.0f ,0.5f ,0.2f ,1.0f);\n"
+	"  FragColor = vec4(ourColor ,1.0f);\n"
 	"}\n\0";
 
 int main () {
@@ -84,9 +88,10 @@ int main () {
 	glDeleteShader(fragmentShader);
 	
 	float vertices[] = {
-		-0.5f ,-0.5f ,0.0f
-		,0.5f ,-0.5f ,0.0f
-		,0.0f ,0.5f ,0.0f
+		// position	// color
+		0.5f ,-0.5f ,0.0f ,1.0f ,0.0f ,0.0f
+		,-0.5f ,-0.5f ,0.0f ,0.0f ,1.0f ,0.0f
+		,0.0f ,0.5f ,0.0f ,0.0f ,0.0f ,1.0f
 	};
 
 	unsigned int VBO ,VAO;
@@ -96,18 +101,18 @@ int main () {
 	glBindBuffer(GL_ARRAY_BUFFER ,VBO);
 	glBufferData(GL_ARRAY_BUFFER ,sizeof(vertices) ,vertices ,GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0 ,3 ,GL_FLOAT ,GL_FALSE ,3 * sizeof(float) ,(void*)0);
+	glVertexAttribPointer(0 ,3 ,GL_FLOAT ,GL_FALSE ,6 * sizeof(float) ,(void*)0);
 	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER ,0);
+	glVertexAttribPointer(1 ,3 ,GL_FLOAT ,GL_FALSE ,6 * sizeof(float) ,(void*)(3*sizeof(float)));
+	glEnableVertexAttribArray(1);
 
-	glBindVertexArray(0);
+	glUseProgram(shaderProgram);
 
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 		glClearColor(0.2f ,0.3f ,0.3f ,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES ,0 ,3);
 
