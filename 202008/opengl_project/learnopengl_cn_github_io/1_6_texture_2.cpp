6b4463bf2b811dc896846a8ce72c3ff5576e075e
sk95120 <765871277@qq.com>
2021-01-04 15:46:25 +0800

    1. add texture mulitp values;

--- 202008/opengl_project/learnopengl_cn_github_io/1_6_texture_2.cpp
+++ 202008/opengl_project/learnopengl_cn_github_io/1_6_texture_2.cpp
@@ -42,7 +42,7 @@ int main () {
 		std::cout << "Success initialize GLAD" << std::endl;	
 	}
 
-	Shader ourShader("./raw/4.1.texture.vs" ,"./raw/4.1.texture.fs");
+	Shader ourShader("./raw/4.2.texture.vs" ,"./raw/4.2.texture.fs");
 
 	float vertices[] = {
 		// position			// color			// texture coords
@@ -77,7 +77,7 @@ int main () {
 	glEnableVertexAttribArray(2);
 
 	// load and create a texture
-	unsigned int texture;
+	unsigned int texture ,texture2;
 	glGenTextures(1 ,&texture);
 	glBindTexture(GL_TEXTURE_2D ,texture);
 	
@@ -88,6 +88,7 @@ int main () {
 	glTexParameteri(GL_TEXTURE_2D ,GL_TEXTURE_MAG_FILTER ,GL_LINEAR);
 	
 	int width ,height ,nrChannels;
+	stbi_set_flip_vertically_on_load(true);
 	unsigned char* data = stbi_load("/home/shike/Documents/computerVisionDraft/202008/opengl_project/learnopengl_cn_github_io/drawable/container.jpg" ,&width ,&height ,&nrChannels ,0);
 	if (data) {
 		glTexImage2D(GL_TEXTURE_2D ,0 ,GL_RGB ,width ,height ,0 ,GL_RGB ,GL_UNSIGNED_BYTE ,data);
@@ -96,12 +97,36 @@ int main () {
 		std::cout << "Failed to load texture" << std::endl;
 	}
 	stbi_image_free(data);
+
+	// texture 2
+	glGenTextures(1 ,&texture2);
+	glBindTexture(GL_TEXTURE_2D ,texture2);
+	glTexParameteri(GL_TEXTURE_2D ,GL_TEXTURE_WRAP_S ,GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D ,GL_TEXTURE_WRAP_T ,GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D ,GL_TEXTURE_MIN_FILTER ,GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D ,GL_TEXTURE_MAG_FILTER ,GL_LINEAR);
+
+	data = stbi_load("/home/shike/Documents/computerVisionDraft/202008/opengl_project/learnopengl_cn_github_io/drawable/awesomeface.png" ,&width ,&height ,&nrChannels ,0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D ,0 ,GL_RGBA ,width ,height ,0 ,GL_RGBA ,GL_UNSIGNED_BYTE ,data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	} else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+	ourShader.use();
+	glUniform1i(glGetUniformLocation(ourShader.ID ,"texture1") ,0);
+	ourShader.setInt("texture2" ,1);
 	
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 		glClearColor(0.2f ,0.3f ,0.3f ,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D ,texture);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D ,texture2);
 		ourShader.use();
 
 		glBindVertexArray(VAO);
