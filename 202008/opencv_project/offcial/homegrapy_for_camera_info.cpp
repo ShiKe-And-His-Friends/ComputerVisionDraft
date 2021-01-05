61d7994c44f1f77a43cae19e8f0b707a4eb6ab4a
sk95120 <765871277@qq.com>
2021-01-05 21:00:51 +0800

    1. homograpy camera display;

--- 202008/opencv_project/offcial/homegrapy_for_camera_info.cpp
+++ 202008/opencv_project/offcial/homegrapy_for_camera_info.cpp
@@ -61,5 +61,60 @@ void homographyFromCameraDisplacement(const string& img1Path ,const string& img2
 	Mat img1 = imread(samples::findFile(img1Path));
 	Mat img2 = imread(samples::findFile(img2Path));
 	vector<Point2f> corners1 ,corners2;
-	bool found1 = findChessboardCorners(img1);
+	bool found1 = findChessboardCorners(img1 ,patternSize ,corners1);
+	bool found2 = findChessboardCorners(img2 ,patternSize ,corners2);
+	
+	if (!found1 || !found2) {
+		cout << "Error ,cannot find the chessboard corners in both images." << endl;
+		return;
+	}
+	
+	vector<Point3f> objectPoints;
+	calcChessboardCorners(patternSize ,squareSize ,objectPoints);
+	
+	FileStorage fs(samples::findFile(intrinsicesPath) ,FileStorage::READ);
+	Mat cameraMatrix ,distCoeffs;
+	fs["camera_matrix"] >> cameraMatrix;
+	fs["distortion_coefficients"] >> distCoeffs;
+	
+	Mat rvec1 ,tvec1;
+	solvePnP(objectPoints ,corners1 ,cameraMatrix ,distCoeffs ,rvec1 ,tvec1);
+	Mat rvec1 ,tvec2;
+	solvePnP(objectPoints ,corners2 ,cameraMatrix ,distCoeffs ,rvec2 ,tvec2);
+	
+	Mat img1_copy_pose = img1.clone();
+	Mat img2_copy_pose = img2.clone();
+	Mat img_draw_poses;
+	drawFrameAxes(img1_copy_pose ,cameraMatrix ,distCoeffs ,rvec1 ,tvec1 ,2 * squareSize);
+	drawFrameAxes(img2_copy_pose ,cameraMatrix ,distCoeffs ,rvec2 ,tvec2 , 2 * squareSize);
+	hconcat(img1_copy_pose ,img2_copy_pose ,img_draw_poses);
+	imshow("Chessboard poses" ,img_draw_poses);
+	
+	Mat R1 ,R2;
+	Rodrigues(rvec1 ,R1);
+	Rodrigues(rvec2 ,R2);
+	
+	Mat R_1to2 ,t_1to2;
+	computeC2MC1(R1 ,tvec1 ,R2 ,tvec2 ,R_1to2 ,t_1to2);
+	Mat rvec_1to2;
+	Rodrigues(R_1to2 ,rvec_1to2);
+	
+	Mat normal = (Mat_<double>(3 ,1) << 0 ,1 ,1);
+	Mat normal1 = R1 * normal;
+	
+	Mat origin(3 ,1 ,CV_64F ,Scalar(0));
+	Mat origin1 = R1 * origin + tvec1;
+	double d_inv1 = 1.0 / normal1.dot(origin1);
+	
+	Mat homograph_euclidean = computeHomography(R_1to2 ,t_1to2 ,d_inv ,normal1);
+	Mat homography = cameraMatrix * homograph_euclidean * cameraMatrix.inv();
+	
+	homograph /= homograph.at<double>(2 ,2);
+	homograph_euclidean /= homograph_euclidean.at<double>(2 ,2);
+	
+	// same but using absolute camera poses instead of camera displacement ,just for check
+	Mat homograph_euclidean2 = computeHomography(R1 ,tvec1 ,R2 ,tvec2 ,d_inv1 ,normal1);
+	Mat homograph2 = cameraMatrix * homograph_euclidean2 * cameraMatrix.inv();
+	
+
 }
