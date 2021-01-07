0ab8c25b27126ce284cc96dd3e13872bbc427be3
sk95120 <765871277@qq.com>
2021-01-07 15:27:21 +0800

    1. add tranform code that rotate before tranform;

--- 202008/opengl_project/learnopengl_cn_github_io/1_7_transferm.cpp
+++ 202008/opengl_project/learnopengl_cn_github_io/1_7_transferm.cpp
@@ -136,9 +136,9 @@ int main () {
 		
 
 		glm::mat4 transform = glm::mat4(1.0f);
-		transform = glm::translate(transform ,glm::vec3(0.5f ,-0.5f ,0.0f));
-		transform = glm::rotate(transform ,(float)glfwGetTime() ,glm::vec3(0.0f ,0.0f ,1.0f));
 
+		transform = glm::rotate(transform ,(float)glfwGetTime() ,glm::vec3(0.0f ,0.0f ,1.0f));
+		transform = glm::translate(transform ,glm::vec3(0.5f ,-0.5f ,0.0f));
 		ourShader.use();
 
 		unsigned int transformLoc = glGetUniformLocation(ourShader.ID ,"transform");
