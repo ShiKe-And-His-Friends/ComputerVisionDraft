2a4a3a033ba9a8db8d19c07d437e61e32734a8c3
sk95120 <765871277@qq.com>
2021-01-09 14:42:19 +0800

    1 .add camera coordinate code that look at;

--- 202008/opengl_project/learnopengl_cn_github_io/1_9_camera_1.cpp
+++ 202008/opengl_project/learnopengl_cn_github_io/1_9_camera_1.cpp
@@ -50,32 +50,74 @@ int main () {
 		std::cout << "Success initialize GLAD" << std::endl;	
 	}
 
-	Shader ourShader("./raw/6.1.coordinate.vs" ,"./raw/6.1.coordinate.fs");
+	Shader ourShader("./raw/7.1.coordinate.vs" ,"./raw/7.1.coordinate.fs");
 
 	float vertices[] = {
 		// position			// texture coords			
-		0.5f ,0.5f ,0.0f 	,1.0f ,1.0f
-		,0.5f ,-0.5f ,0.0f 	,1.0f ,0.0f
-		,-0.5f ,-0.5f ,0.0f ,0.0f ,0.0f
-		,-0.5f ,0.5f ,0.0f	,0.0f ,1.0f
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 	
-	unsigned int indices[] = {
-		0 ,1 ,3
-		,1 ,2 ,3
+	glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3 (2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
 
-	unsigned int VBO ,VAO ,EBO;
+	unsigned int VBO ,VAO;
 	glGenVertexArrays(1 ,&VAO);
 	glGenBuffers(1 ,&VBO);
-	glGenBuffers(1 ,&EBO);
 	
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER ,VBO);
 	glBufferData(GL_ARRAY_BUFFER ,sizeof(vertices) ,vertices ,GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER ,EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER ,sizeof(indices) ,indices ,GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0 ,3 ,GL_FLOAT ,GL_FALSE ,5 * sizeof(float) ,(void*)0);
 	glEnableVertexAttribArray(0);
@@ -124,10 +166,14 @@ int main () {
 	ourShader.setInt("texture1" ,0);
 	ourShader.setInt("texture2" ,1);
 	
+	// pass postion matrix to shader
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f) ,(float)SRC_HEIGHT ,0.1f ,100.0f);
+	ourShader.setMat4("projection" ,projection);
+	
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 		glClearColor(0.2f ,0.3f ,0.3f ,1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D ,texture);
@@ -136,24 +182,22 @@ int main () {
 		
 		ourShader.use();
 
-		glm::mat4 model = glm::mat4(1.0f);
 		glm::mat4 view = glm::mat4(1.0f);
-		glm::mat4 projection = glm::mat4(1.0f);
-
-		model = glm::rotate(model ,glm::radians(-55.0f) ,glm::vec3(1.0f ,0.0f ,0.0f));
-		view = glm::translate(view ,glm::vec3(0.0f ,0.0f ,-3.0f));
-		projection = glm::perspective(glm::radians(45.0f) ,(float)SRC_WIDTH / (float)SRC_HEIGHT ,0.1f ,100.0f);
-
-		unsigned int modelLoc = glGetUniformLocation(ourShader.ID ,"model");
-		unsigned int viewLoc = glGetUniformLocation(ourShader.ID ,"view");
-
-		glUniformMatrix4fv(modelLoc ,1 ,GL_FALSE ,glm::value_ptr(model));
-		glUniformMatrix4fv(viewLoc ,1 ,GL_FALSE ,&view[0][0]);
-
-		ourShader.setMat4("projection" ,projection);
-
-		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES ,6 ,GL_UNSIGNED_INT ,0);
+		float radians = 10.0f;
+		float camX = sin(glfwGetTime()) * radians;
+		float camY = cos(glfwGetTime()) * radians;
+		view = glm::lookAt(glm::vec3(camX ,0.0f ,camZ) ,glm::vec3(0.0f ,0.0f ,0.0f) ,glm::vec3(0.0f ,1.0f ,0.0f));
+		ourShader.setMat4("view" ,view);
+	
+		glBindVertexArray(AVO);
+		for (unsigned int i = 0 ; i < 10 ;i++) {
+			glm::vec4 model = glm::mat4(1.0f);
+			model = glm::translate(model ,cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model ,glm::radians(angle) ,glm::vec3(1.0f ,0.3f ,0.5f));
+			ourShader.setMat4("model" ,model);
+			glDrawArrays(GL_TRIANGLES ,0 ,36);
+		}
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -161,7 +205,6 @@ int main () {
 
 	glDeleteVertexArrays(1 ,&VAO);
 	glDeleteBuffers(1 ,&VBO);
-	glDeleteBuffers(1 ,&EBO);
 
 	glfwTerminate();
 	return 0;
