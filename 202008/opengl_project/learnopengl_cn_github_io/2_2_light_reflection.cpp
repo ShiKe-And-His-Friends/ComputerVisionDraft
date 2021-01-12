c4203ca4460d3eeab432e9cf57d693779d59a52f
sk95120 <765871277@qq.com>
2021-01-12 15:57:16 +0800

    1. add color light that ambient and diffuse

--- 202008/opengl_project/learnopengl_cn_github_io/2_2_light_reflection.cpp
+++ 202008/opengl_project/learnopengl_cn_github_io/2_2_light_reflection.cpp
@@ -127,7 +127,7 @@ int main () {
 	
 	glBindBuffer(GL_ARRAY_BUFFER ,VBO);
 	
-	glVertexAttribPointer(0 ,3 ,GL_FLOAT ,GL_FALSE ,3*sizeof(float) ,(void*)0);
+	glVertexAttribPointer(0 ,3 ,GL_FLOAT ,GL_FALSE ,6*sizeof(float) ,(void*)0);
 	glEnableVertexAttribArray(0);
 	
 	while(!glfwWindowShouldClose(window)) {
--- 202008/opengl_project/learnopengl_cn_github_io/raw/2.1.basic_lighting.fs
+++ 202008/opengl_project/learnopengl_cn_github_io/raw/2.1.basic_lighting.fs
@@ -16,7 +16,8 @@ void main() {
 	// diffuse
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(lightPos - FragPos);
-	float diffuse = diff * lightColor;
+	float diff = max(dot(norm ,lightDir) ,0.0);
+	vec3 diffuse = diff * lightColor;
 	
 	vec3 result = (ambient + diffuse) * objectColor;
 	FragColor = vec4(result ,1.0);
