3087a8852544baf50e314230e0c307f85dd7e423
sk95120 <765871277@qq.com>
2021-01-13 15:12:48 +0800

    fix bug: 1. change caster directional rotate;

--- 202008/opengl_project/learnopengl_cn_github_io/2_5_light_cast_directional.cpp
+++ 202008/opengl_project/learnopengl_cn_github_io/2_5_light_cast_directional.cpp
@@ -200,7 +200,7 @@ int main () {
 		for (unsigned int i = 0 ; i < 10 ; i++) {
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model ,cubePositions[i]);
-			float angle = 20.0f * i;
+			float angle = 20.0f * glfwGetTime();
 			model = glm::rotate(model ,glm::radians(angle) ,glm::vec3(1.0f ,0.3f ,0.5f));
 			lightingShader.setMat4("model" ,model);
 			glDrawArrays(GL_TRIANGLES ,0 ,36);
