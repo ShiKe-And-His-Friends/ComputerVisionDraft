89ae85756df8068db28ab62fcc4756e88d9bb8f2
sk95120 <765871277@qq.com>
2021-01-13 11:55:17 +0800

    1. add color light that map diffuse exerice 2;

--- 202008/opengl_project/learnopengl_cn_github_io/raw/4.1.lighting_maps_exerices_1.fs
+++ 202008/opengl_project/learnopengl_cn_github_io/raw/4.1.lighting_maps_exerices_1.fs
@@ -2,28 +2,30 @@
 out vec4 FragColor;
 
 struct Material {
-	sampler2D diffuse;
-	vec3 specular;
-	float shininess;
+    sampler2D diffuse;
+    sampler2D specular;
+    float     shininess;
+};  
+
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
 };
 
-struct Light{
-	vec3 position;
-	vec3 ambient;
-	vec3 diffuse;
-	vec3 specular;
-};
-
-in vec3 FragPos;
-in vec3 Normal;
+in vec3 FragPos;  
+in vec3 Normal;  
 in vec2 TexCoords;
-
+  
 uniform vec3 viewPos;
 uniform Material material;
 uniform Light light;
 
-void main() {
- // ambient
+void main()
+{
+    // ambient
     vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
   	
     // diffuse 
@@ -38,5 +40,5 @@ void main() {
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * spec * (vec3(1.0) - vec3(texture(material.specular, TexCoords))); // here we inverse the sampled specular color. Black becomes white and white becomes black.
         
-    FragColor = vec4(ambient + diffuse + specular, 1.0);  	
-}
+    FragColor = vec4(ambient + diffuse + specular, 1.0);  
+} 
