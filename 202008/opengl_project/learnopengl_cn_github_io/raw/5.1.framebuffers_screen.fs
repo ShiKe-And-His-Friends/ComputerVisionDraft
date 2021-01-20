3e76f8915f925080db9e5296a099e4836f149872
sk95120 <765871277@qq.com>
2021-01-20 20:39:07 +0800

    1. depth draw that frame buffer change 2;

--- 202008/opengl_project/learnopengl_cn_github_io/raw/5.1.framebuffers_screen.fs
+++ 202008/opengl_project/learnopengl_cn_github_io/raw/5.1.framebuffers_screen.fs
@@ -7,5 +7,7 @@ uniform sampler2D screenTexture;
 
 void main()
 {
-    FragColor = vec4(vec3(1.0 - texture(screenTexture, TexCoords)), 1.0);
+    FragColor = texture(screenTexture, TexCoords);
+    float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
+    FragColor = vec4(average, average, average, 1.0);
 } 
