1fe73796b37de7ce787a270ba026a02e5f4599b9
sk95120 <765871277@qq.com>
2021-01-20 19:34:58 +0800

    1. depth draw that add shader;

--- 202008/opengl_project/learnopengl_cn_github_io/4_3_blend_discard.cpp
+++ 202008/opengl_project/learnopengl_cn_github_io/4_3_blend_discard.cpp
@@ -172,6 +172,15 @@ int main () {
     unsigned int floorTexture = loadTexture("./drawable/metal.png");
     unsigned int transparentTexture = loadTexture("./drawable/grass.png");
 	
+	 vector<glm::vec3> vegetation 
+    {
+        glm::vec3(-1.5f, 0.0f, -0.48f),
+        glm::vec3( 1.5f, 0.0f, 0.51f),
+        glm::vec3( 0.0f, 0.0f, 0.7f),
+        glm::vec3(-0.3f, 0.0f, -2.3f),
+        glm::vec3 (0.5f, 0.0f, -0.6f)
+    };
+	
     shader.use();
     shader.setInt("texture1", 0);
 	
