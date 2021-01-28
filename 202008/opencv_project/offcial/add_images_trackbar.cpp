c4a0f7b0c6904a5c02247e63028e280826d26a98
sk95120 <765871277@qq.com>
2021-01-28 15:03:24 +0800

    1. add image view shadow code ;

--- 202008/opencv_project/offcial/add_images_trackbar.cpp
+++ 202008/opencv_project/offcial/add_images_trackbar.cpp
@@ -1,29 +1,34 @@
-#include "opencv2/imgcodes.hpp"
-#include "opencv2/heighgui.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include <iostream>
 
 using namespace cv;
-using std::count;
+using std::cout;
 
-const int alpha_slidar_max = 100;
+const int alpha_slider_max = 100;
 int alpha_slider;
 double alpha;
 double beta;
-
 Mat src1;
 Mat src2;
 Mat dst;
 
 static void on_trackbar(int ,void*) {
-	alpha = (double)alpha_slider / alpha_slidar_max;
+	alpha = (double)alpha_slider/alpha_slider_max;
 	beta = (1.0 - alpha);
-	addWighted(src1 ,alpha ,src2 ,beta ,0.0 ,dst);
+	addWeighted(src1 ,alpha ,src2 ,beta ,0.0 ,dst);
 	imshow("Linear Blend" ,dst);
 }
 
 int main(void) {
+	/*
+	  1. add global envrionment:
+	  export OPENCV_SAMPLES_DATA_PATH=/home/shike/Documents/opencv-4.2.0/samples/data
+	  2. add class comiple environment
+	cv::samples::addSamplesDataSearchPath("/home/shike/Documents/opencv-4.2.0/samples/data");
+	*/
 	src1 = imread(samples::findFile("LinuxLogo.jpg"));
 	src2 = imread(samples::findFile("WindowsLogo.jpg"));
-	
 	if (src1.empty()) {
 		cout << "Error loading src1 \n";
 		return -1;
@@ -33,12 +38,12 @@ int main(void) {
 		return -1;
 	}
 	alpha_slider = 0;
-	namedWindow("Linear Blend" ,WINDOW_AUTOSIZE ,alpha_slidar_max);
-	
+	namedWindow("Linear Blend" ,WINDOW_AUTOSIZE);
 	char TrackbarName[50];
 	sprintf(TrackbarName ,"Alpha x %d" ,alpha_slider_max);
-	createTrackbar( TrackbarName, "Linear Blend", &alpha_slider, alpha_slider_max, on_trackbar );
-	on_trackbar( alpha_slider, 0 );
+	createTrackbar(TrackbarName ,"Linear Blend" ,&alpha_slider ,alpha_slider_max ,on_trackbar);
+	on_trackbar(alpha_slider ,0);
+
 	waitKey(0);
-	return 0;
+	return 0;	
 }
