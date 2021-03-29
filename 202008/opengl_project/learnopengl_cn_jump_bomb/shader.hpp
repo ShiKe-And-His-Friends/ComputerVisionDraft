9f792f4008473a5e91195d6c28ad508bd8a3aa27
sk95120 <765871277@qq.com>
2021-03-29 10:08:56 +0800

    1. create opengl jump bomb basic code;

---
+++ 202008/opengl_project/learnopengl_cn_jump_bomb/shader.hpp
@@ -0,0 +1,18 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <string>
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Shader {
+
+public:
+	GLuint ID;
+
+
+};
+
+#endif
