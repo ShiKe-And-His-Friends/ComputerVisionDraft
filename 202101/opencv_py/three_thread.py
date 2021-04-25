1932d276dbf3352a4ef6e4771afd8e5468e39236
sk95120 <765871277@qq.com>
2021-04-25 15:07:11 +0800

    1. update python muliti thread codes;

--- 202101/opencv_py/three_thread.py
+++ 202101/opencv_py/three_thread.py
@@ -1,39 +1,59 @@
 #!/usr/bin/python3
 
+import queue
 import threading
 import time
 
+exitFlag = 0
+
 class myThread(threading.Thread):
-    def __init__(self ,threadID ,name ,counter):
+    def __init__(self ,threadID ,name ,q):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
-        self.counter = counter
+        self.q = q
 
     def run(self):
         print("start thread:" + self.name)
-        threadLock.acquire()
-        print_time(self.name ,self.counter ,3)
-        threadLock.release()
+        process_data(self.name ,self.q)
         print("exit thread:" + self.name)
 
-def print_time(threadName ,delay ,counter):
-    while counter:
-        time.sleep(delay)
-        print("%s : %s" %(threadName ,time.ctime(time.time())))
-        counter -= 1
+def process_data(threadName ,q):
+    while not exitFlag:
+        queueLock.acquire()
+        if not workQueue.empty():
+            data = q.get()
+            queueLock.release()
+            print("%s processing %s" %(threadName ,data))
+        else:
+            queueLock.release()
+        time.sleep(1)
 
-threadLock = threading.Lock()
+threadList = ["Thread-1" ,"Thread-2" ,"Thread-3"]
+nameList = ["One" ,"Two" ,"Three" ,"Four" ,"Five"]
+queueLock = threading.Lock()
+workQueue = queue.Queue(10)
 threads = []
+threadID = 1
 
-thread1 = myThread(1 ,"Thread-1" ,1)
-thread2 = myThread(2 ,"Thread-2" ,2)
+for tName in threadList:
+    thread = myThread(threadID ,tName ,workQueue)
+    thread.start()
+    threads.append(thread)
+    threadID += 1
 
-thread1.start()
-thread2.start()
-threads.append(thread1)
-threads.append(thread2)
+queueLock.acquire()
+for word in nameList:
+    workQueue.put(word)
+queueLock.release()
+
+while not workQueue.empty():
+    pass
+
+exitFlag = 1
+
 for t in threads:
     t.join()
+
 print("exit main thread")
 
