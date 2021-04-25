1439554642e8f09ad9c5616ca0e1c2f3dcea5a64
sk95120 <765871277@qq.com>
2021-04-25 14:49:58 +0800

    1. python muliti thread codes;

--- 202101/opencv_py/three_thread.py
+++ 202101/opencv_py/three_thread.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
 
-import _thread
+import threading
 import time
 
-def print_time(threadName ,delay) :
-    count = 0
-    while count < 5:
+exitFlag = 0
+
+class myThread(threading.Thread):
+    def __init__(self ,threadID ,name ,counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+
+    def run(self):
+        print("start thread:" + self.name)
+        print_time(self.name ,self.counter ,5)
+        print("exit thread:" + self.name)
+
+def print_time(threadName ,delay ,counter):
+    while counter:
+        if exitFlag:
+            threadName.exit()
         time.sleep(delay)
-        count += 1
         print("%s : %s" %(threadName ,time.ctime(time.time())))
+        counter -= 1
 
-try:
-    _thread.start_new_thread(print_time ,("Thread-1" ,2,))
-    _thread.start_new_thread(print_time ,("Thread-2" ,4,))
-except:
-    print("Erroe:No Thread Start")
+thread1 = myThread(1 ,"Thread-1" ,1)
+thread2 = myThread(2 ,"Thread-2" ,2)
 
-while 1:
-    pass
+thread1.start()
+thread2.start()
+thread1.join()
+thread2.join()
+print("exit main thread")
+
