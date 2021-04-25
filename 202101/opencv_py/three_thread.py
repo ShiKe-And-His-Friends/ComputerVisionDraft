87286a4ec23cd0a1d2e2f3c9bb4240bc87faa091
sk95120 <765871277@qq.com>
2021-04-25 14:55:07 +0800

    1. update python muliti thread codes;

--- 202101/opencv_py/three_thread.py
+++ 202101/opencv_py/three_thread.py
@@ -3,8 +3,6 @@
 import threading
 import time
 
-exitFlag = 0
-
 class myThread(threading.Thread):
     def __init__(self ,threadID ,name ,counter):
         threading.Thread.__init__(self)
@@ -14,23 +12,28 @@ class myThread(threading.Thread):
 
     def run(self):
         print("start thread:" + self.name)
-        print_time(self.name ,self.counter ,5)
+        threadLock.acquire()
+        print_time(self.name ,self.counter ,3)
+        threadLock.release()
         print("exit thread:" + self.name)
 
 def print_time(threadName ,delay ,counter):
     while counter:
-        if exitFlag:
-            threadName.exit()
         time.sleep(delay)
         print("%s : %s" %(threadName ,time.ctime(time.time())))
         counter -= 1
+
+threadLock = threading.Lock()
+threads = []
 
 thread1 = myThread(1 ,"Thread-1" ,1)
 thread2 = myThread(2 ,"Thread-2" ,2)
 
 thread1.start()
 thread2.start()
-thread1.join()
-thread2.join()
+threads.append(thread1)
+threads.append(thread2)
+for t in threads:
+    t.join()
 print("exit main thread")
 
