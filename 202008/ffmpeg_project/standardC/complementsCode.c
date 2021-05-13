a5ab55d2682d096f55bff542cf736b595cf2cb20
sk95120 <765871277@qq.com>
2021-05-13 16:16:31 +0800

    1. death loop codes;

--- 202008/ffmpeg_project/standardC/complementsCode.c
+++ 202008/ffmpeg_project/standardC/complementsCode.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#define MAX 255
 
-int main() {
+int compoments_codes() {
 	char a = -1;
 	signed char b = -1;
 	unsigned char c = -1;
 	printf("a=%d ,b=%d ,c=%d" ,a ,b ,c);
+	return 0;
+}
+
+int main() {
+	unsigned char a[MAX], i;
+	for (i = 0; i < MAX ; i++) {
+		a[i] = i;
+		for (i = 0; i < MAX;i++) {
+			printf("%d ",a[i]);
+		}
+	}
 	return 0;
 }
