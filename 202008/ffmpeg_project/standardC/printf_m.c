bb807277ee551c9b2ac4ff2fe6218f7659b33405
sk95120 <765871277@qq.com>
2021-05-14 18:18:14 +0800

    1. update printf codes;

---
+++ 202008/ffmpeg_project/standardC/printf_m.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <windows.h>
+#include <assert.h>
+
+int printf_m(const char *format , ...) {
+	asserts(foramt);
+}
+
+int main() {
+	printf_m("w\n");
+	printf_m("1234\n");
+	printf_m("abcdef\n");
+
+	system("pause");
+	return 0;
+}
