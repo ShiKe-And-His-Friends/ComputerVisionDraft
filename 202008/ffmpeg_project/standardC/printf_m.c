e5024a0f6edff2ccf675d36ee5740323068df1de
sk95120 <765871277@qq.com>
2021-05-17 09:42:41 +0800

    1. update printf codes;

--- 202008/ffmpeg_project/standardC/printf_m.c
+++ 202008/ffmpeg_project/standardC/printf_m.c
@@ -2,28 +2,47 @@
 #include <windows.h>
 #include <assert.h>
 
+is_print(int n) {
+	while (n>0) {
+		is_print(n / 10);
+		putchar(n % 10 + '0');
+	}
+}
+
 int printf_m(const char *format , ...) {
-	asserts(format);
+	assert(format);
 	va_list arg;		
 	va_start(arg, format);
 	char *p = format;
 	while (*p){
-		if (*p == '%')
+		if (*p == '%') {
 			p++;
-		else putchar(*p);
+		}
+		else {
+			putchar(*p);
+		}
 		p++;
 	}
 	switch (*p){
-	    case 'd': is_print(va_arg(arg, int));break;
+	    case 'd':
+			is_print(va_arg(arg, int));
+			break;
+
 	    case 's':{
-			 char *start = va_arg(arg, char*);  
-			    while (*start){					
-				 putchar(*start);
-				 start++;
-				 }
-		}; break;
-		case 'c':putchar(va_arg(arg, char)); break;
-		default: break;
+			char *start = va_arg(arg, char*);  
+			while (*start){
+				putchar(*start);
+				start++;
+			}
+		}; 
+			break;
+
+		case 'c':
+			putchar(va_arg(arg, char));
+			break;
+
+		default:
+			break;
 	}
 	return format;
 }
--- 202101/opencv_py/socket_server.py
+++ 202101/opencv_py/socket_server.py
@@ -6,6 +6,7 @@ class server_ssl:
     def build_listen(self):
         context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
         context.load_cert_chain('cert/server.crt' ,'cert/server_rsa_private.pem.unsecure')
+        ssl.match_hostname('cert/server.crt' ,'127.0.0.1');
         with socket.socket(socket.AF_INET ,socket.SOCK_STREAM ,0) as sock:
             sock.bind(('127.0.0.1' ,9443))
             sock.listen(5)
