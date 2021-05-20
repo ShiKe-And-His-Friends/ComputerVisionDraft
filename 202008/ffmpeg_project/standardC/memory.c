2a82d9803c63395ccdf05a4d02fc06b634bcc678
sk95120 <765871277@qq.com>
2021-05-20 20:19:17 +0800

    1. add memory copy codes;

--- 202008/ffmpeg_project/standardC/memory.c
+++ 202008/ffmpeg_project/standardC/memory.c
@@ -27,24 +27,18 @@ void* my_memmove(void *dest ,void *str ,int size) {
 	void* result = dest;
 
 	if (dest <= str || (char*)dest >= (char*)str + size) {
-		printf("YYYYYYYYY\n", size);
+		printf("From header coverage.\n", size);
 		while (size --) {
-			printf("%d\n", size);
 			*(char *)dest = *(char *)str;
-			printf("%c\n", *(char*)str);
-			printf("%c\n", *(char*)dest);
 			(char *)dest = (char *)dest + 1;
 			(char *)str = (char *)str + 1;
 		}
 	} else {
-		printf("NNNNNNNNN\n", size);
-		dest = (char *)dest + size - 3;
-		str = (char*)str + size - 3;
+		printf("From tailer coverage.\n", size);
+		dest = (char *)dest + size - 1;
+		str = (char*)str + size - 1;
 		while (size--) {
-			printf("%d\n" ,size);
-			printf("%c\n", *(char*)str);
 			*(char*)dest = *(char*)str;
-			printf("%c\n", *(char*)dest);
 			(char*)dest = (char*)dest - 1;
 			(char*)str = (char*)str - 1;
 		}
@@ -52,37 +46,46 @@ void* my_memmove(void *dest ,void *str ,int size) {
 	return result;
 }
 
+void* mymemchr(const void *dest ,char c, int size) {
+	assert(dest);
+	assert(size > 0);
+	while (size && *(char *)dest) {
+		if (*(char *)dest == (char) c) {
+			return (char *)dest;
+		}
+		((char*)dest)++;
+		size--;
+	}
+	return 0;
+}
+
 int main() {
 	char* p = NULL;
 	char* q = NULL;
-	char data[100];
-	char data2[100] = "asdfghjkl";
+	char data[10] = { "asdfghjkl" };
+	char data2[10];
 	
-	int len = 9;
-	//int len = strlen(p);
-	q = (char *)data;
-	p = (char*)data2;
+	// initilize
+	p = (char*)data;
+	//q = (char *)data2;
 
-	//p = (q + 1);
-	q = (p - 1);
+	int len = strlen(p);
+
+	q = (p + 1);
 	printf("%p\n", p);
 	printf("%p\n", q);
-	/*char tt = q;
+
+	// solve bytes coverage
+	q = my_memmove(q ,p ,len);
+	char* tt = q;
 	for (int i = 0; i < len; i++) {
 		printf("%c", *q);
 		q++;
 	}
-	printf("\n");
-	q = tt;*/
-	printf("%p\n", p);
-	printf("%p\n", q);
-	q = my_memmove(q ,p ,len);
-	printf("\n");
-	printf("NNN");
-	printf("\n");
-	q = my_memmove(p ,q ,len);
-	printf("XXX");
+	q = tt;
 	printf("\n");
 
+	q = mymemchr(q, 'k' ,len);
+	printf("letter is %c\n" ,*q);
 	return 0;
 }
