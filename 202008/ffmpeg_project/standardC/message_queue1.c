0e6baf1b84f6fe876854206a72c354b4987daf1a
sk95120 <765871277@qq.com>
2021-05-27 20:47:25 +0800

    1. update thread codes;

--- 202008/ffmpeg_project/standardC/message_queue1.c
+++ 202008/ffmpeg_project/standardC/message_queue1.c
@@ -1,10 +1,7 @@
 #include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <signal.h>
 
 #define KEY 1024
