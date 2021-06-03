8ec8573d2ee1188c2997bb01e0ce9b8196ae1864
sk95120 <765871277@qq.com>
2021-06-03 16:55:08 +0800

    1. add rtp codes;

---
+++ 202008/ffmpeg_project/rtp/main.c
@@ -0,0 +1,18 @@
+/*
+ Thanks for Github: https://github.com/lmshao/RTP
+ Created by Liming Shao on 2018/5/11.
+ Copy And Use In My Project.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <RtpEnc.h>
+
+int main() {
+	printf("Rtp program start.\n");
+
+
+
+	printf("Rtp program stop.\n");
+	return 0;
+}
