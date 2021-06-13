3f74e5c0c1a3a838b83d10583590cb7e705f70ea
sk95120 <765871277@qq.com>
2021-06-13 16:54:57 +0800

    1. update rtp codes;

---
+++ 202008/ffmpeg_project/rtpv2/send_h264_file_rtp.c
@@ -0,0 +1,26 @@
+/**
+* Thanks for https://github.com/hmgle/h264_to_rtp
+* Copy from it.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "send_h264_file_rtp.h"
+
+void add_client(linklist client_ip_list ,char*  ipaddr) {
+	struct sockaddr_in server_c;
+	pnode pnode_tmp;
+	const int on = 1;
+
+	insert_nodelp_node(client_ip_list ,ipaddr);
+	pnode_temp = search_node(CLIENT_IP_LIST ,ipaddr);
+	server_c.sin_family = AF_INET;
+	server_c.sin_port = htons(DEST_PORT);
+
+	if ((connect(pnode_temp->node_info.socket_c ,(const struct sockaddr *)&server_c)) == -1) {
+		perror("connect");
+		exit(-1);
+	}
+}
+
+static int copy_nal_from_file(FILE *p ,uint8_t *buf ,int *len);
