e1dc95e4e13fced8c6914a4e9996bce20283bc88
sk95120 <765871277@qq.com>
2021-06-17 10:08:17 +0800

    1. update inv sqrt codes;

--- 202008/ffmpeg_project/standardC/invt_sqrt.c
+++ 202008/ffmpeg_project/standardC/invt_sqrt.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+	4
+	0.499154
+	1.996614
+	16
+	0.249577
+	3.993229
+	999999
+	0.000998
+	998.304016
+	1111111
+	0.000948
+	1053.417236
+*/
+
+static float invSqrt(float x) {
+	float xhalf = 0.5 * x;
+	int i = *(int *)&x;
+	i = 0x5f3759df - (i >> 1);
+	x = *(float *) & i;
+	x = x * (1.5 - xhalf * x * x);
+	return x;
+}
+
 int main() {
 
 	char buf[1024];
@@ -11,14 +35,19 @@ int main() {
 			continue;
 		}
 		int index = length - 1;
-		int multiple = 10;
-		int num = buf[index] - 48;
+		float multiple = 10;
+		float num = buf[index] - 48;
 		for (index -- ; index >= 0 ; index --)
 		{
 			num += (buf[index] - 48) * multiple;
 			multiple *= 10;
 		}
-	
+		
+		float inv_sqrt = invSqrt(num);
+		printf("%f\n" ,inv_sqrt);
+
+		float sqrt = invSqrt(num) * num;
+		printf("%f\n", sqrt);
 	}
 
 	return 0;
