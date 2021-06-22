1a7af411c9220f2a2bbfcbda9e9429cd2303efb0
sk95120 <765871277@qq.com>
2021-06-22 14:43:00 +0800

    1. update plot codes;

--- 202101/tf/keras_fashion_mnist.py
+++ 202101/tf/keras_fashion_mnist.py
@@ -36,11 +36,54 @@ model = keras.Sequential([
 model.compile(optimizer = 'adam',
         loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True),
         metrics = ['accuracy'])
-print("train fashion mnist model:")
+print("Train fashion mnist model:")
 model.fit(train_images ,train_labels ,epochs = 10)
 print("Test fashion mnist model:")
 test_loss ,test_acc = model.evaluate(test_images ,test_labels ,verbose = 2)
 print("\naccuracy:" ,test_acc)
+print("Predict model:")
+probality_model = tf.keras.Sequential([model ,
+    tf.keras.layers.Softmax()])
+predictions = probality_model.predict(test_images)
+# print(predictions[0]);
+maxPredictValue = np.argmax(predictions[0])
+print(maxPredictValue)
+
+def plot_image(i ,predictions_array ,true_label ,img):
+    predictions_array ,true_label ,img = predictions_array ,true_label[i] ,img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(img ,cmap = plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+    plt.xlabel("{}{:2.0f}%({})".format(class_names[predicted_label],
+        100 * np.max(predictions_array),
+        class_names[true_label],
+        color = color))
+
+def plot_value_array(i ,predictions_array ,true_label):
+    predictions_array ,true_label = predictions_array ,true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10) ,predictions_array ,color = "#777777")
+    plt.ylim([0 ,1])
+    predicted_label = np.argmax(predictions_array)
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+i = 12
+plt.figure(figsize=(6 ,3))
+plt.subplot(1 ,2 ,1)
+plot_image(i ,predictions[i] ,test_labels ,test_images)
+plt.subplot(1 ,2 ,2)
+plot_value_array(i ,predictions[i] ,test_labels)
+plt.show()
 
 print("compile fashion mnist model done")
 
