780f2f11e23b84d28b2e0da1390a546822c297d9
sk95120 <765871277@qq.com>
2021-06-22 12:11:14 +0800

    1. update codes;

--- 202101/tf/keras_fashion_mnist.py
+++ 202101/tf/keras_fashion_mnist.py
@@ -8,10 +8,21 @@ print(tf.__version__)
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images ,train_labels) ,(test_images ,test_labels) = fashion_mnist.load_data()
 
-print(len(train_labels))
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
+# print(len(train_labels))
+
+class_names = ['T-shirt/top' ,'Trouser' ,'Pullover' ,'Dress' ,'Coat' ,'Sandal' ,'Shirt'
+        ,'Sneaker' ,'Bag' ,'Ankle boot']
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize = (10 ,10))
+for i in range(25):
+    plt.subplot(5 ,5 ,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i] ,cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
 plt.show()
 
