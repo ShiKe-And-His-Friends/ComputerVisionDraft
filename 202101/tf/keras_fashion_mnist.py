7609ad4bad658bef6b440eab49b265442c29e76b
sk95120 <765871277@qq.com>
2021-06-22 12:28:41 +0800

    1. update train/test model codes;

--- 202101/tf/keras_fashion_mnist.py
+++ 202101/tf/keras_fashion_mnist.py
@@ -16,6 +16,7 @@ class_names = ['T-shirt/top' ,'Trouser' ,'Pullover' ,'Dress' ,'Coat' ,'Sandal' ,
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
+'''
 plt.figure(figsize = (10 ,10))
 for i in range(25):
     plt.subplot(5 ,5 ,i+1)
@@ -25,4 +26,21 @@ for i in range(25):
     plt.imshow(train_images[i] ,cmap=plt.cm.binary)
     plt.xlabel(class_names[train_labels[i]])
 plt.show()
+'''
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape = (28 ,28)),
+    keras.layers.Dense(128 ,activation = 'relu'),
+    keras.layers.Dense(10)
+    ])
+model.compile(optimizer = 'adam',
+        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True),
+        metrics = ['accuracy'])
+print("train fashion mnist model:")
+model.fit(train_images ,train_labels ,epochs = 10)
+print("Test fashion mnist model:")
+test_loss ,test_acc = model.evaluate(test_images ,test_labels ,verbose = 2)
+print("\naccuracy:" ,test_acc)
+
+print("compile fashion mnist model done")
 
