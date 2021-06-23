d41383e7bea36a1c818ade480681ad6b1988e365
sk95120 <765871277@qq.com>
2021-06-23 14:29:26 +0800

    1. update regresssion codes;

--- 202101/tf/regression_predict_fuel_enfficiency.py
+++ 202101/tf/regression_predict_fuel_enfficiency.py
@@ -65,9 +65,29 @@ def build_model():
 model = build_model()
 print(model.summary())
 # train model
+example_batch = normed_train_data[:10]
+example_result= model.predict(example_batch)
+print(example_result)
+class PrintDot(keras.callbacks.Callback):
+    def on_epoch_end(self ,epoch ,logs):
+        if epoch % 100 == 0:
+            print('')
+        print('.',end='')
+EPOCHS = 1000
+
+history = model.fit(
+        normed_train_data,
+        train_labels,
+        epochs = EPOCHS ,
+        validation_split = 0.2,
+        verbose = 0,
+        callbacks = [PrintDot()])
+hist = pd.DataFrame(history.history)
+hist['epoch'] = history.epoch
+print(hist.tail())
 
 # evaluate model
 
 # draw result
 
-print("Regession predict fuel efficiency done.")
+print("\nRegession predict fuel efficiency done.\n")
