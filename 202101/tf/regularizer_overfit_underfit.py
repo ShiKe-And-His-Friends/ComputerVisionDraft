6acf05c50cbad5a9b3497b478be73396aa1ec6ee
sk95120 <765871277@qq.com>
2021-06-23 15:22:49 +0800

    1. add regularize codes;

---
+++ 202101/tf/regularizer_overfit_underfit.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras import regularizers
+import tensorflow_docs as tfdocs
+import tensorflow_docs.modeling
+import tensorflow_docs.plots
+from IPython import display
+from matplotlib import pyplot as plt
+import numpy as np
+import pathlib
+import shutil
+import tempfile
+
+print(tf.__version__)
+
+# download baseline
+
+# prepare data
+
+# demonstrate overfit
+
+# draw 
+
+# strategies 
+
+# regularize
+
+# dropout 
+
+# draw
+
+
