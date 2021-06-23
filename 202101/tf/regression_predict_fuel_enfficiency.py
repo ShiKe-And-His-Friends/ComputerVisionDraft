7c3484d4b027d0dafea4964dbe767da75b943273
sk95120 <765871277@qq.com>
2021-06-23 14:44:17 +0800

    1. update regresssion codes;

--- 202101/tf/regression_predict_fuel_enfficiency.py
+++ 202101/tf/regression_predict_fuel_enfficiency.py
@@ -86,6 +86,32 @@ hist = pd.DataFrame(history.history)
 hist['epoch'] = history.epoch
 print(hist.tail())
 
+def plot_history(history):
+    hist = pd.DataFrame(history.history)
+    hist['epoch'] = history.epoch
+    
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Abs Error [MPG]')
+    plt.plot(hist['epoch'] ,hist['mae'],
+            label = 'Val Error')
+    plt.plot(hist['epoch'] ,hist['val_mae'],
+            label = 'Val Error')
+    plt.ylim([0 ,5])
+    plt.legend()
+    
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Square Error [$MPG^2$]')
+    plt.plot(hist['epoch'] ,hist['mse'],
+            label = 'Train Error')
+    plt.plot(hist['epoch'],hist['val_mse'],
+            label = 'Val Error')
+    plt.ylim([0 ,20])
+    plt.legend()
+    plt.show()
+plot_history(history)
+
 # evaluate model
 
 # draw result
