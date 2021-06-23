148d9215a044a0ff9ec1beeffe47ae80ff92e2ca
sk95120 <765871277@qq.com>
2021-06-23 11:42:46 +0800

    1. update hub codes];

--- 202101/tf/hub_imdb_movice.py
+++ 202101/tf/hub_imdb_movice.py
@@ -19,11 +19,42 @@ train_data ,validation_data ,test_data = tfds.load(
         as_supervised = True)
 
 # print datas
+train_examples_batch ,train_labels_batch = next(iter(train_data.batch(10)))
+print(train_examples_batch)
+print(train_labels_batch)
+
+# build model
+'''
+Google Embedding Text Model
+https://hub.tensorflow.google.cn/google/tf2-preview/gnews-swivel-20dim/1
+google/tf2-preview/gnews-swivel-20dim-with-oov/1
+google/tf2-preview/nnlm-en-dim50/1
+google/tf2-preview/nnlm-en-dim128/1
+'''
+embedding = "https://hub.tensorflow.google.cn/google/tf2-preview/gnews-swivel-20dim/1"
+hub_layer = hub.KerasLayer(embedding ,input_shape = [],
+        dtype = tf.string ,trainable = True)
+print(hub_layer(train_examples_batch[:3]))
+model = tf.keras.Sequential()
+model.add(hub_layer)
+model.add(tf.keras.layers.Dense(16 ,activation = 'relu'))
+model.add(tf.keras.layers.Dense(1))
+model.summary()
 
 # train model
+model.compile(optimizer = 'adam',
+        loss = tf.keras.losses.BinaryCrossentropy(from_logits = True),
+        metrics = ['accuracy'])
+history = model.fit(train_data.shuffle(10000).batch(512),
+        epochs = 20,
+        validation_data = validation_data.batch(512),
+        verbose = 1)
 
 # evalate model
+results = model.evaluate(test_data.batch(512) ,verbose = 2 )
+for name ,value in zip(model.metrics_names , results):
+    print("%s : %.3f" % (name ,value))
 
 # draw result
 
-print("Hub train imdb movices's model done.")
+print("\nHub train imdb movices's model done.\n")
