0daaa10dfe49844e758cf2df57c9a590d0a56a8e
sk95120 <765871277@qq.com>
2021-06-23 12:30:27 +0800

    1. add regresssion codes;

--- 202101/tf/regression_predict_fuel_enfficiency.py
+++ 202101/tf/regression_predict_fuel_enfficiency.py
@@ -1,6 +1,6 @@
 import pathlib
 import matplotlib.pyplot as plt
-import panndas as pd
+import pandas as pd
 import seaborn as sns
 import tensorflow as tf
 from tensorflow import keras
@@ -9,11 +9,61 @@ from tensorflow.keras import layers
 print(tf.__version__)
 
 # download baseline
+dataset_path = keras.utils.get_file("auto-mpg.data" ,"http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
+dataset_path
+column_names = ['MPG' ,'Cylinders' ,'Displacement' ,'Horsepower' ,'Weight' ,'Acceleration' ,'Model Year' ,'Origin']
+raw_dataset = pd.read_csv(dataset_path ,names = column_names ,
+        na_values = "?" ,
+        comment = '\t',
+        sep = " ",
+        skipinitialspace = True)
+dataset = raw_dataset.copy()
 
 # print data
+print(dataset.tail())
+
+# clean data
+print(dataset.isna().sum())
+dataset = dataset.dropna()
+origin = dataset.pop('Origin') # one-hot
+dataset['USA'] = (origin == 1) * 1.0
+dataset['Europe'] = (origin == 2) * 1.0
+dataset['Japan'] = (origin == 3) * 1.0
+print(dataset.tail())
+
+# data examine
+train_dataset = dataset.sample(frac = 0.8 ,random_state = 0)
+test_dataset = dataset.drop(train_dataset.index)
+text1 = sns.pairplot(train_dataset[["MPG" ,"Cylinders" ,"Displacement" ,"Weight"]] ,diag_kind = "kde")
+print(text1)
+train_stats = train_dataset.describe()
+train_stats.pop("MPG")
+train_stats = train_stats.transpose()
+print(train_stats)
+
+# extract feature
+train_labels = train_dataset.pop('MPG')
+test_labels = test_dataset.pop('MPG')
+def norm(x):
+    return (x - train_stats['mean']) / train_stats['std']
+normed_train_data = norm(train_dataset)
+normed_test_data = norm(test_dataset)
 
 # build model
+def build_model():
+    model = keras.Sequential([
+        layers.Dense(64 ,activation = 'relu' ,input_shape = [len(train_dataset.keys())]),
+        layers.Dense(64 ,activation = 'relu'),
+        layers.Dense(1)])
 
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
+
+    model.compile(loss='mse',
+            optimizer = optimizer,
+            metrics = ['mae' ,'mse'])
+    return model
+model = build_model()
+print(model.summary())
 # train model
 
 # evaluate model
