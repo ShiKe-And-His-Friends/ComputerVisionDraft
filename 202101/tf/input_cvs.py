020f91f59958a5acda7dcd139cdfe50e12a5c04d
sk95120 <765871277@qq.com>
2021-06-29 11:22:27 +0800

    1. update input images codes;

--- 202101/tf/input_cvs.py
+++ 202101/tf/input_cvs.py
@@ -34,6 +34,8 @@ abalone_model.fit(
     epochs = 10
 )
 normalize = preprocessing.Normalization()
+print("\n\n\n 0:\n")
+print(abalone_features)
 normalize.adapt(abalone_features)
 norm_abalone_model = tf.keras.Sequential([
     normalize,
@@ -55,28 +57,44 @@ titanic = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.
 print(titanic.head())
 titanic_features = titanic.copy()
 titanic_labels = titanic_features.pop("survived")
-mInput = tf.keras.Input(shape =() ,dtype = tf.float32)
-result = 2 * mInput + 1
+input = tf.keras.Input(shape =() ,dtype = tf.float32)
+result = 2 * input + 1
 print(result)
-calc = tf.keras.Model(inputs = mInput ,outputs = result)
+calc = tf.keras.Model(inputs = input ,outputs = result)
 print(calc(1).numpy())
 print(calc(2).numpy())
 
 inputs = {}
 for name ,column in titanic_features.items():
     dtype = column.dtype
-    if type == object:
+    if dtype == object:
         dtype = tf.string
     else:
         dtype = tf.float32
 
     inputs[name] = tf.keras.Input(shape = (1 ,) ,name = name ,dtype = dtype)
 print(inputs)
-numeric_inputs = {name:mInput for name ,mInput in inputs.items()
-    if mInput.dtype == tf.float32}
+numeric_inputs = {name:input for name ,input in inputs.items()
+    if input.dtype == tf.float32}
 x = layers.Concatenate()(list(numeric_inputs.values()))
 norm = preprocessing.Normalization()
-norm.adapt(np.array(titanic[numeric_inputs.keys()]).astype('float32'))
+
+print("\n\n\n 1:\n")
+y1 = numeric_inputs.keys()
+print(y1)
+y2 = titanic[y1]
+print("\n\n\n 2:\n")
+print(y2)
+y3 = np.array(y2)
+print("\n\n\n 3:\n")
+print(y3)
+print("\n\n\n 4:\n")
+# y4 = tf.cast(y3 ,dtype = tf.float32)
+y4 = y3.astype(np.float32)
+print(y4)
+norm.adapt(y3)
+
+# norm.adapt(np.array(titanic[numeric_inputs.keys()]))
 # norm.adapt(titanic_features)
 all_numeric_inputs = norm(x)
 print(all_numeric_inputs)
