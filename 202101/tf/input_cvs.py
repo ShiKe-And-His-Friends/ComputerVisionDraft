af8fc501437093e8c0006a074836fa5028aa3db3
sk95120 <765871277@qq.com>
2021-06-29 18:32:14 +0800

    1. update cvs input;

--- 202101/tf/input_cvs.py
+++ 202101/tf/input_cvs.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental import preprocessing
+import itertools
 
 np.set_printoptions(precision = 3 ,suppress = True)
 
@@ -78,27 +79,120 @@ numeric_inputs = {name:input for name ,input in inputs.items()
     if input.dtype == tf.float32}
 x = layers.Concatenate()(list(numeric_inputs.values()))
 norm = preprocessing.Normalization()
-
-print("\n\n\n 1:\n")
-y1 = numeric_inputs.keys()
-print(y1)
-y2 = titanic[y1]
-print("\n\n\n 2:\n")
-print(y2)
-y3 = np.array(y2)
-print("\n\n\n 3:\n")
-print(y3)
-print("\n\n\n 4:\n")
-# y4 = tf.cast(y3 ,dtype = tf.float32)
-y4 = y3.astype(np.float32)
-print(y4)
-norm.adapt(y3)
-
-# norm.adapt(np.array(titanic[numeric_inputs.keys()]))
-# norm.adapt(titanic_features)
+norm.adapt(np.array(titanic[numeric_inputs.keys()]))
 all_numeric_inputs = norm(x)
 print(all_numeric_inputs)
 preprocessed_inputs = [all_numeric_inputs]
+
+for name ,input in inputs.items():
+    if input.dtype == tf.float32:
+        continue
+    lookup = preprocessing.StringLookup(vocabulary = np.unique(titanic_features[name]))
+    one_hot = preprocessing.CategoryEncoding(max_tokens = lookup.vocab_size())
+
+    x= lookup(input)
+    x = one_hot(x)
+    preprocessed_inputs.append(x)
+print(preprocessed_inputs)
+preprocessed_inputs_cat = layers.Concatenate()(preprocessed_inputs)
+titanic_preprocessing = tf.keras.Model(inputs ,preprocessed_inputs_cat)
+
+# draw model layer png
+tf.keras.utils.plot_model(model = titanic_preprocessing ,rankdir = "LR" ,dpi = 72 ,show_shapes = True)
+
+titanic_features_dict = {name : np.array(value) 
+    for name ,value in titanic_features.items()}
+titanic_preprocessing(titanic_features_dict)
+feature_dict = {name:values[:1] for name ,values in titanic_features_dict.items()}
+print(titanic_preprocessing(feature_dict))
+
+def titanic_model(preprocessing_head ,inputs):
+    body = tf.keras.Sequential([
+        layers.Dense(64),
+        layers.Dense(1)
+    ])
+    preprocessed_inputs = preprocessing_head(inputs)
+    result = body(preprocessed_inputs)
+    model = tf.keras.Model(inputs ,result)
+    model.compile(loss = tf.losses.BinaryCrossentropy(from_logits = True),
+        optimizer = tf.optimizers.Adam())
+    return model
+titanic_model = titanic_model(titanic_preprocessing ,inputs)
+titanic_model.fit(
+    x = titanic_features_dict,
+    y = titanic_labels,
+    epochs = 10
+)
+titanic_model.save('titanic_test')
+reloaded = tf.keras.models.load_model('titanic_test')
+feature_dict = {name:values[:1] for name ,values in titanic_features_dict.items()}
+before = titanic_model(feature_dict)
+after = reloaded(feature_dict)
+assert(before - after) < 1e-3
+print(before)
+print(after)
+
+# in memory data
+def slices(features):
+    for i in itertools.count():
+        example = {name:values[i] for name ,values in features.items()}
+        yield example
+for example in slices(titanic_features_dict):
+    for name ,value in example.items():
+        print(f"{name:19s}:{value}")
+    break
+titanic_ds = tf.data.Dataset.from_tensor_slices((titanic_features_dict ,titanic_labels))
+titanic_batchs = titanic_ds.shuffle(len(titanic_labels)).batch(32)
+titanic_model.fit(
+    titanic_batchs,
+    epochs = 5
+)
+
+titanic_file_path = tf.keras.utils.get_file("train.csv" ,"https://storage.googleapis.com/tf-datasets/titanic/train.csv")
+titanic_csv_ds = tf.data.experimental.make_csv_dataset(
+    titanic_file_path,
+    batch_size = 5,
+    label_name = 'survived',
+    num_epochs = 1,
+    ignore_errors = True,
+)
+for batch ,label in titanic_csv_ds.take(1):
+    for key ,value in batch.items():
+        print(f"{key:20s}:{value}")
+    print()
+    print(f"{'label':20s}:{label}")
+
+traffic_volume_csv_gz = tf.keras.utils.get_file(
+    'Metro_Interstate_Traffic_Volume.csv.gz',
+    "https://archive.ics.uci.edu/ml/machine-learning-databases/00492/Metro_Interstate_Traffic_Volume.csv.gz",
+    cache_dir = '.',
+    cache_subdir = 'traffic'
+)
+traffic_volume_csv_gz_ds = tf.data.experimental.make_csv_dataset(
+    traffic_volume_csv_gz,
+    batch_size = 256,
+    label_name = 'traffic_volume',
+    num_epochs = 1,
+    compression_type = "GZIP"
+)
+for batch ,label in traffic_volume_csv_gz_ds.take(1):
+    for key ,value in batch.items():
+        print(f"{key:20s}:{value[:5]}")
+    print()
+    print(f"{'label':20s}:{label[:5]}")
+for i ,(batch ,label) in enumerate(traffic_volume_csv_gz_ds.repeat(20)):
+    if i % 40 == 0:
+        print('.',end = '')
+print()
+snapshot = tf.data.experimental.snapshot('titanic.tfsnap')
+snapshotting = traffic_volume_csv_gz_ds.apply(snapshot).shuffle(1000)
+for i ,(batch ,label) in enumerate(snapshotting.shuffle(1000).repeat(20)):
+    if i % 40 == 0:
+        print('.' ,end='')
+print()
+
+# multiple character
+
 
 print("\nInput CVS data done.\n")
 
