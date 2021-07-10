c17ec506abc03de74344cb0d1dc2a25420350b57
sk95120 <765871277@qq.com>
2021-07-10 15:24:59 +0800

    1. custmoizate tensor codes;

---
+++ 202101/tf/customizate_tensor.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+import numpy as np
+import time
+
+print(tf.add(1 ,2))
+print(tf.add([1 ,2] ,[3 ,4]))
+print(tf.square(5))
+print(tf.reduce_sum([1 ,2 ,3]))
+print(tf.square(2) + tf.square(3))
+x = tf.matmul([[1]] ,[[2 ,3]])
+print(x)
+print(x.shape)
+print(x.dtype)
+
+ndarray = np.ones([3 ,3])
+print("TensorFlow operations convert numpy arrays to Tensors automatically.")
+tensor = tf.multiply(ndarray ,42)
+print(tensor)
+print("And Numpy opertations convert Tensors to numpy arrays automatically.")
+print(np.add(tensor ,1))
+print("The .numpy() methods explicitly convert a Tensor to a numpy array.")
+print(tensor.numpy())
+x = tf.random.uniform([3 ,3])
+print("Is there a GPU availabel:"),
+print(tf.config.list_physical_devices("GPU"))
+print("Is the Tensor on CPU #0: "),
+print(x.device.endswith('GPU:0'))
+def time_matmul(x):
+    start = time.time()
+    for loop in range(10):
+        tf.matmul(x ,x)
+    result = time.time() - start
+    print("10 loops:{:0.2f}ms".format(1000 * result))
+print("On CPU:")
+with tf.device("CPU:0"):
+    x = tf.random.uniform([1000 ,1000])
+    assert x.device.endswith("CPU:0")
+    time_matmul(x)
+
+if tf.config.list_physical_devices("GPU"):
+    print("On GPU:")
+    with tf.device("GPU:0"):
+        x = tf.random.uniform([1000 ,1000])
+        assert x.device.endswith("GPU:0")
+        time_matmul(x)
+
+
+print("Customizate tensor done.")
