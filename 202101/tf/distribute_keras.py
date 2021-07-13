91c4e51158e92a45dd6e548b2f8cf586df3620ff
sk95120 <765871277@qq.com>
2021-07-13 11:38:32 +0800

    1. update distribute keras codes;

---
+++ 202101/tf/distribute_keras.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import os
+
+print(tf.__version__)
+datasets ,info = tfds.load(name = 'mnist' ,with_info = True ,as_supervised = True)
+mnist_train ,mnist_test = datasets['train'] ,datasets['test']
+strategy = tf.distribute.MirroredStrategy()
+print('Number of devices:{}'.format(strategy.num_replicas_in_sync))
+
+num_train_example = info.splits['train'].num_examples
+num_test_example = info.splits['test'].num_examples
+BUFFER_SIZE = 10000
+BATCH_SIZE_PER_REPLICA = 64
+BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
+
+def scale(image ,label):
+    image = tf.cast(image ,tf.float32)
+    image /= 255
+    return image ,label
+train_dataset = mnist_train.map(scale).cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+eval_dataset = mnist_test.map(scale).batch(BATCH_SIZE)
+with strategy.scope():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(
+            32,
+            3,
+            activation = 'relu',
+            input_shape = (28 ,28 ,1)
+        ),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(
+            64 ,
+            activation = 'relu'
+        ),
+        tf.keras.layers.Dense(10)
+    ])
+    model.compile(
+        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True),
+        optimizer = tf.keras.optimizers.Adam(),
+        metrics = ['accuracy']
+    )
+
+    checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir ,"ckpt_{epoch}")
+def decay(epoch):
+    if epoch < 3:
+        return 1e-3
+    elif epoch >= 3 and epoch < 7:
+        return 1e-4
+    else:
+        return 1e-5
+class PrintLR(tf.keras.callbacks.Callback):
+    def on_epoch_end(self ,epoch ,logs = None):
+        print('\nLearning rate for epoch{} is {}'.format(
+            epoch + 1,
+            model.optimizer.lr.numpy()))
+callbacks = [
+    tf.keras.callbacks.TensorBoard(log_dir = './log')
+]
+
+
+print("Distribute keras done.")
