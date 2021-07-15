b9ee907ec94293bbb1491134c0bac41a5f8ea2b2
sk95120 <765871277@qq.com>
2021-07-15 10:26:57 +0800

    1. update distirbute multi keras codes;

--- 202101/tf/distribute_multi_keras.py
+++ 202101/tf/distribute_multi_keras.py
@@ -14,4 +14,15 @@ single_worker_dataset = distribute_multi_mnist.mnist_dataset(batch_size)
 single_worker_model = distribute_multi_mnist.build_and_compile_cnn_model()
 single_worker_model.fit(single_worker_dataset ,epochs = 3 ,steps_per_epoch = 70)
 
+tf_config = {
+    'cluster':{
+        'worker':['localhost:12345' ,'localhost:23456']
+    },
+    'task':{'type':'worker' ,'index':0}
+}
+print(json.dumps(tf_config))
+os.environ['GREETINGS'] = 'Hello Tensorflow'
+
+strategy = tf.distribute.MultiWorkerMirroredStrategy()
+
 print("Distribute multi keras done.")
