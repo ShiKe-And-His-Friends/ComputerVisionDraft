c1ca1cb8490f8ebb2951b9d002172ca1ba4cae05
sk95120 <765871277@qq.com>
2021-07-15 10:11:07 +0800

    1. update distirbute multi keras codes;

---
+++ 202101/tf/distribute_multi_keras.py
@@ -0,0 +1,17 @@
+import json
+import os
+import sys
+import tensorflow as tf
+import distribute_multi_mnist
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ.pop('TF_CONFIG' ,None)
+
+if '.' not in sys.path:
+    sys.path.insert(0 ,'.')
+batch_size = 64
+single_worker_dataset = distribute_multi_mnist.mnist_dataset(batch_size)
+single_worker_model = distribute_multi_mnist.build_and_compile_cnn_model()
+single_worker_model.fit(single_worker_dataset ,epochs = 3 ,steps_per_epoch = 70)
+
+print("Distribute multi keras done.")
