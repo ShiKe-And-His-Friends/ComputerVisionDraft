e8bc54f1dd76055123746d991dbfd34a895e3842
sk95120 <765871277@qq.com>
2021-07-18 21:53:16 +0800

    1. update images classify codes;

--- 202101/tf/image_classify_basic.py
+++ 202101/tf/image_classify_basic.py
@@ -7,9 +7,18 @@ print(tf.__version__)
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images ,train_labels) ,(test_images ,test_labels) = fashion_mnist.load_data()
 class_names = [
-    'T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-    'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'
+    'airplane', 'automobile', 'bird', 'cat', 'deer',
+    'dog', 'frog', 'horse', 'ship', 'truck'
 ]
+plt.figure(figsize = (10 ,10))
+for i in range(25):
+    plt.subplot(5 ,5 ,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i])
+    plt.xlabel(class_names[train_labels[i][0]])
+plt.show()
 train_images.shape
 test_images.shape
 
