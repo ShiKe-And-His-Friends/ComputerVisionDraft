f2bf671c85009e84363144c8280f0e0a808a9bd4
sk95120 <765871277@qq.com>
2021-07-19 09:17:23 +0800

    1. update images classify codes;

--- 202101/tf/image_cnn.py
+++ 202101/tf/image_cnn.py
@@ -3,3 +3,6 @@ from tensorflow.keras import datasets ,layers ,models
 import matplotlib.pyplot as plt
 
 (train_images ,train_labels),(test_images ,test_labels) = datasets.cifar10.load_data()
+
+model = models.Sequential()
+model.add(layers.Conv2D(32 ,(3 ,3) ,avtivation = 'relu' ,input_shape = (32 ,32 ,3)))
