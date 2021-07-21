8bd74e2998322561dba553dec0e7e1e75291e895
sk95120 <765871277@qq.com>
2021-07-21 12:05:54 +0800

    1. update images classify basic codes;

--- 202101/tf/image_classify_basic.py
+++ 202101/tf/image_classify_basic.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import os
 import PIL
 import tensorflow as tf
+from tensorflow.keras import layers ,Sequential
 import pathlib
 
 print(tf.__version__)
@@ -52,5 +53,35 @@ for image_batch ,labels_batch in train_ds:
     print(image_batch.shape)
     print(labels_batch.shape)
     break
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size = AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size = AUTOTUNE)
+normallization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x,y : (normallization_layer(x),y))
+image_batch ,labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+print(np.min(first_image) ,np.max(first_image))
+
+num_classes = 5
+model = Sequential([
+    layers.experimental.preprocessing.Rescaling(1./255 ,input_shape = (img_height ,img_width ,3)),
+    layers.Conv2D(16 ,3 ,padding = 'same' ,activation = 'relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32 ,3 ,padding = 'same' ,activation = 'relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64 ,3 ,padding = 'same' ,activation = 'relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128 ,activation = 'relu'),
+    layers.Dense(num_classes)
+])
+model.compile(
+    optimizer = 'adam',
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True),
+    metrics = ['accuracy']
+)
+print(model.summary())
+
 
 print("Image classify basic done.")
