0e3ce39dcc58d2b26f18d7ef202d138d750df6f7
sk95120 <765871277@qq.com>
2021-07-21 10:26:31 +0800

    1. update images classify basic codes;

--- 202101/tf/image_classify_basic.py
+++ 202101/tf/image_classify_basic.py
@@ -1,25 +1,56 @@
-import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import PIL
+import tensorflow as tf
+import pathlib
 
 print(tf.__version__)
 
-fashion_mnist = tf.keras.datasets.fashion_mnist
-(train_images ,train_labels) ,(test_images ,test_labels) = fashion_mnist.load_data()
-class_names = [
-    'airplane', 'automobile', 'bird', 'cat', 'deer',
-    'dog', 'frog', 'horse', 'ship', 'truck'
-]
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file('flower_photos' ,origin = dataset_url ,untar = True)
+data_dir = pathlib.Path(data_dir)
+image_count = len(list(data_dir.glob('*/*.jpg')))
+print(image_count)
+roses = list(data_dir.glob('roses/*'))
+PIL.Image.open(str(roses[0]))
+print(str(roses[0]))
+
+tulips = list(data_dir.glob('tulips/*'))
+PIL.Image.open(str(tulips[0]))
+batch_size = 32
+img_height = 180
+img_width = 180
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split = 0.2,
+    subset = "training",
+    seed = 123,
+    image_size = (img_height ,img_width),
+    batch_size = batch_size
+)
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split = 0.2,
+    subset = "validation",
+    seed = 123,
+    image_size = (img_height ,img_width),
+    batch_size = batch_size
+)
+class_names = train_ds.class_names
+print(class_names)
+
 plt.figure(figsize = (10 ,10))
-for i in range(25):
-    plt.subplot(5 ,5 ,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i])
-    plt.xlabel(class_names[train_labels[i][0]])
-plt.show()
-train_images.shape
-test_images.shape
+for images ,labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3 ,3 ,i+1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+# plt.show()
+for image_batch ,labels_batch in train_ds:
+    print(image_batch.shape)
+    print(labels_batch.shape)
+    break
 
 print("Image classify basic done.")
