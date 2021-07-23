695995ac39d0166721d57b8ac6616206dda3384b
sk95120 <765871277@qq.com>
2021-07-23 12:22:19 +0800

    1. text embedding codes;

--- 202101/tf/word_embedding.py
+++ 202101/tf/word_embedding.py
@@ -51,5 +51,67 @@ print(result.numpy())
 result = embedding_layer(tf.constant([[0 ,1 ,2] ,[3 ,4 ,5]]))
 print(result.shape)
 
+def custom_standardization(input_data):
+    lowercase = tf.strings.lower(input_data)
+    stripped_html = tf.strings.regex_replace(lowercase ,'<br />', ' ')
+    return tf.strings.regex_replace(
+        stripped_html,
+        '[%s]' % re.escape(string.punctuation),
+        ''
+    )
+vocab_size = 10000
+sequence_length = 100
+vectorize_layer = TextVectorization(
+    standardize = custom_standardization,
+    max_tokens = vocab_size,
+    output_mode = 'int',
+    output_sequence_length = sequence_length
+)
+text_ds = train_ds.map(lambda x ,y : x)
+vectorize_layer.adapt(text_ds)
+embedding_dim = 18
+model = Sequential([
+    vectorize_layer,
+    Embedding(
+        vocab_size,
+        embedding_dim,
+        name = "embedding"
+    ),
+    GlobalAveragePooling1D(),
+    Dense(16 ,activation = 'relu'),
+    Dense(1)
+])
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir = "logs")
+model.compile(
+    optimizer = 'adam',
+    loss = tf.keras.losses.BinaryCrossentropy(from_logits = True),
+    metrics = ['accuracy']
+)
+model.fit(
+    train_ds,
+    validation_data = val_ds,
+    epochs = 15,
+    callbacks = [tensorboard_callback]
+)
+print(model.summary())
+
+weights = model.get_layer('embedding').get_weights()[0]
+vocab = vectorize_layer.get_vocabulary()
+out_v = io.open('vectors.tsv' ,'w' ,encoding = 'utf-8')
+out_m = io.open('metadata.tsv' ,'w' ,encoding = 'utf-8')
+for index ,word in enumerate(vocab):
+    if index == 0:
+        continue
+    vec = weights[index]
+    out_v.write('\t'.join([str(x) for x in vec]) + "\n")
+    out_m.write(word + "\n")
+out_v.close()
+out_m.close()
+try:
+    from google.colab import files
+    files.download('vectors.tsv')
+    files.download('metadata.tsv')
+except Exception:
+    pass
 
 print("Text Embedding done.")
