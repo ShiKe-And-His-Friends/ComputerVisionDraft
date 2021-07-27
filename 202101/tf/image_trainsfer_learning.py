213f5697e5f559bd8ebe84f9ae91ee7bceb4c9ab
sk95120 <765871277@qq.com>
2021-07-27 10:30:36 +0800

    1. update images transfer learning codes;

--- 202101/tf/image_trainsfer_learning.py
+++ 202101/tf/image_trainsfer_learning.py
@@ -24,4 +24,14 @@ validation_dataset = image_dataset_from_directory(
     image_size = IMG_SIZE
 )
 
+class_names = train_dataset.class_names
+plt.figure(figsize = (10 ,10))
+for images ,labels in train_dataset.take(1):
+    for i in range(9):
+        ax = plt.subplot(3 ,3 ,i+1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+plt.show()        
+
 print("Image transfer learning done,")
