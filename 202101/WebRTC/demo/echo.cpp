b71fbdb7896a6fad89847fb2364930bba2dfd362
sk95120 <765871277@qq.com>
2021-08-01 09:39:58 +0800

    1. update echo eliminate codes;

--- 202101/WebRTC/demo/echo.cpp
+++
@@ -1,9 +0,0 @@
-#include "webrtc/modules/audio_processing/aec/include/echo_cancellation.h"
-
-int16_t *samples = (int16_t *)buffer->push();
-if(samples == NULL){
-	log_debug("drop oldest recorded frame");
-	buffer->pop();
-	samples = (int16_t *)buffer->push();
-	assert(samples);
-}
