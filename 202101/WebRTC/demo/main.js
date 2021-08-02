20eb64bf9c67e5cb68f0f66884773e38709cbeb5
sk95120 <765871277@qq.com>
2021-08-02 09:47:16 +0800

    1. format codes;

--- 202101/WebRTC/demo/main.js
+++ 202101/WebRTC/demo/main.js
@@ -1,18 +1,17 @@
-
 function hasUserMedia() {
-  return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+	return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
 }
 if (hasUserMedia()) {
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;   
-  navigator.getUserMedia({
-    video: true,
-    audio: true
-  }, function (stream) {
-    var video = document.querySelector('video');
-    video.src = window.URL.createObjectURL(stream);
-  }, function (err) {});
+	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;   
+	navigator.getUserMedia({
+		video: true,
+		audio: true
+	}, function (stream) {
+		var video = document.querySelector('video');
+		video.src = window.URL.createObjectURL(stream);
+	}, function (err) {});
 } else {
-  alert("抱歉，你的浏览器不支持 getUserMedia.");
+	alert("Expoler not support getUserMedia.");
 }
 navigator.getUserMedia({video:false ,audio:true } ,function(stream) {
 
