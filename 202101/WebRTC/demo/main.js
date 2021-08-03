c7590632e1e9b40d076db709800019d84dd341ad
sk95120 <765871277@qq.com>
2021-08-03 09:54:01 +0800

    1. update js codes;

--- 202101/WebRTC/demo/main.js
+++ 202101/WebRTC/demo/main.js
@@ -13,6 +13,23 @@ if (hasUserMedia()) {
 } else {
 	alert("Expoler not support getUserMedia.");
 }
-navigator.getUserMedia({video:false ,audio:true } ,function(stream) {
-
-});
+navigator.getUserMedia(
+	{
+		video:{
+			mandatory: {
+				minAspectRation: 1.777,
+				maxAspectRation: 1.778
+			},
+			optional: {
+				maxWidth: 640,
+				maxHeigth: 480
+			}
+		} ,
+		audio:false
+	} ,function(stream) {
+		var video = document.querySelector('video');
+		video.src = window.URL.createObjectURL(stream);
+	} ,function(error) {
+		console.log("Raised an error when capturing" ,error);
+	}
+);
