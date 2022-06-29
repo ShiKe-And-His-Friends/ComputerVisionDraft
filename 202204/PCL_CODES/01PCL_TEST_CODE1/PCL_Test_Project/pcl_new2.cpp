8916f2340bb6cddc01d4c7616cfe3516591ca250
sk95120 <765871277@qq.com>
2022-06-29 13:56:32 +0800

    update rabbit pcl codes

---
+++ 202204/PCL_CODES/01PCL_TEST_CODE1/PCL_Test_Project/pcl_new2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<pcl/visualization/cloud_viewer.h>
+#include<pcl/io/io.h>
+#include<pcl/io/pcd_io.h>//pcd读写类相关的头文件
+#include<pcl/io/ply_io.h>
+#include<pcl/point_types.h> //PCL中支持的点类型头文件
+
+int user_data;
+using std::cout;
+
+
+void viewerOneOff(pcl::visualization::PCLVisualizer& viewer) {
+	//设置背景颜色
+	viewer.setBackgroundColor(1.0, 0.5, 1.0);
+}
+
+int main() {
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+	char strfilepath[256] = "rabbit.pcd";
+	if (-1 == pcl::io::loadPCDFile(strfilepath, *cloud)) {
+		cout << "error input!" << endl;
+		return -1;
+	}
+
+	cout << cloud->points.size() << endl;
+	//创建viewer对象
+	pcl::visualization::CloudViewer viewer("Cloud Viewer");  
+
+	viewer.showCloud(cloud);
+	viewer.runOnVisualizationThreadOnce(viewerOneOff);
+	system("pause");
+	return 0;
+
+}
