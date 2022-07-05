9efaf8c1859f954fddb9631ffd9fd68606b0e772
sk95120 <765871277@qq.com>
2022-07-05 16:59:37 +0800

    change photo byte

--- 202204/OPENCV_CODE/20220629_CalibrateCamera01/20220629_CalibrateCamera01/CameraCalibrate02.cpp
+++ 202204/OPENCV_CODE/20220629_CalibrateCamera01/20220629_CalibrateCamera01/CameraCalibrate02.cpp
@@ -21,30 +21,29 @@ void saveMatInnerData(Mat &gray_Makeself_Photo,Mat &gray_draw_Photo){
 	// draw some what
 	for (int i = 0; i < rowIndex; i++) {
 		for (int j = 0; j < colIndex; j++) {
+			// 1 Byte = 8 bits   1 short = 2 Byte    1 int = 4 Byte
+			Vec4w& bgra_Gray_Makeself = gray_Makeself_Photo.at<Vec4w>(i, j);
+			Vec4w& bgra_Gray_Draw_Photo = gray_draw_Photo.at<Vec4w>(i, j);
+			if (channelsNum == 4) {
+				bgra_Gray_Makeself[3] = (unsigned short)(0);
+			}
+			if (channelsNum >= 1 && channelsNum <= 4) {
+				unsigned short b = bgra_Gray_Makeself[0];
+				unsigned short g = bgra_Gray_Makeself[1];
+				unsigned short r = bgra_Gray_Makeself[2];
+				bgra_Gray_Draw_Photo[0] = saturate_cast<unsigned short>(((float)(colIndex - j) / (float)(colIndex)) * USHRT_MAX); // blue
+				bgra_Gray_Draw_Photo[1] = saturate_cast<unsigned short>(((float)(colIndex - j) / (float)(colIndex)) * USHRT_MAX); // green
+				bgra_Gray_Draw_Photo[2] = saturate_cast<unsigned short>(((float)(rowIndex - i) / (float)(rowIndex)) * USHRT_MAX); // red
+				bgra_Gray_Draw_Photo[3] = saturate_cast<unsigned short>(0.8 * (g + r)); //alpha
+			}
+
 			if (i % 40 == 0 && j % 40 == 0) {
-
 				fileStorage << "rows " << i << "cols " << j;
-
-				// 1 Byte = 8 bits   1 short = 2 Byte    1 int = 4 Byte
-				Vec4w& bgra_Gray_Makeself = gray_Makeself_Photo.at<Vec4w>(i, j);
 				fileStorage << "red before " << bgra_Gray_Makeself[0];
 				fileStorage << "green before " << bgra_Gray_Makeself[1];
 				fileStorage << "blue before " << bgra_Gray_Makeself[2];
 				fileStorage << "alpha before " << bgra_Gray_Makeself[3];
 
-				if (channelsNum == 4) {
-					bgra_Gray_Makeself[3] = (unsigned short)(0);
-				}
-				if (channelsNum >= 1 && channelsNum <= 3) {
-					unsigned short b = bgra_Gray_Makeself[0];
-					unsigned short g = bgra_Gray_Makeself[1];
-					unsigned short r = bgra_Gray_Makeself[2];
-					gray_draw_Photo.at<Vec4w>(i, j)[0] = UCHAR_MAX; // blue
-					gray_draw_Photo.at<Vec4w>(i, j)[1] = saturate_cast<short>( ((float)(colIndex - j) / (float)(colIndex)) * UCHAR_MAX ); // green
-					gray_draw_Photo.at<Vec4w>(i, j)[2] = saturate_cast<short>(((float)(rowIndex - i) / (float)(rowIndex)) * UCHAR_MAX); // red
-					gray_draw_Photo.at<Vec4w>(i, j)[3] = saturate_cast<short>(0.5 * (g + r)); //alpha
-				}
-				Vec4w& bgra_Gray_Draw_Photo = gray_draw_Photo.at<Vec4w>(i, j);
 				// bug: file storage need word instead of number only
 				fileStorage << "red after " << bgra_Gray_Draw_Photo[0];
 				fileStorage << "green after " << bgra_Gray_Draw_Photo[1];
@@ -57,10 +56,15 @@ void saveMatInnerData(Mat &gray_Makeself_Photo,Mat &gray_draw_Photo){
 
 }
 
+// 二值化图片
+void binarilation(Mat& mat) {
+
+}
+
 // 保存图片到本地
-void saveMatData(Mat &gray_Makeself_Photo){
+void saveMatData(Mat &lena_Deepcopy_Photo, Mat &gray_Makeself_Photo, Mat &gray_Draw_Photo){
 	vector<int> params;
-	params.push_back(IMWRITE_JPEG_LUMA_QUALITY);
+	params.push_back(IMWRITE_JPEG_QUALITY);
 	params.push_back(90);
 	params.push_back(IMWRITE_EXR_COMPRESSION_DWAB);
 
@@ -69,7 +73,21 @@ void saveMatData(Mat &gray_Makeself_Photo){
 		success = imwrite("..//CameraData//20220705_gray_makeself.jpg", gray_Makeself_Photo, params);
 	}
 	catch (const Exception& e) {
-		fprintf(stderr, "Exception Write JPEG file error info:", e.what());
+		fprintf(stderr, "Exception Write JPEG file [1] error info:", e.what());
+	}
+
+	try {
+		success = imwrite("..//CameraData//20220705_gray_draw.jpg", gray_Draw_Photo, params);
+	}
+	catch (const Exception& e) {
+		fprintf(stderr, "Exception Write JPEG file [2] error info:", e.what());
+	}
+
+	try {
+		success = imwrite("..//CameraData//20220705_lena_deep_copy.jpg", lena_Deepcopy_Photo, params);
+	}
+	catch (const Exception& e) {
+		fprintf(stderr, "Exception Write JPEG file [3] error info:", e.what());
 	}
 
 	if (success) {
@@ -91,7 +109,6 @@ int main(int argc ,char** argv) {
 	
 	// lena photo deep copy
 	lena.copyTo(lena_Deepcopy_Photo);
-	lena_Deepcopy_Photo;
 
 	namedWindow("lena");
 	imshow("lena", lena_Deepcopy_Photo);
@@ -103,14 +120,19 @@ int main(int argc ,char** argv) {
 	Mat gray_Draw_Photo = Mat(480, 640, CV_16UC4, Scalar(0 ,0 ,128 ,0));
 	imshow("gray windows", gray_Makeself_Photo);
 	waitKey(1000);
+	destroyAllWindows();
 
-	// save debug data
+	// swap
+	binarilation(lena_Deepcopy_Photo);
+
+	// save data
 	saveMatInnerData(gray_Makeself_Photo , gray_Draw_Photo);
 	imshow("gray windows", gray_Draw_Photo);
 	waitKey(2000);
+	destroyAllWindows();
 
 	// save photo
-	saveMatData(gray_Makeself_Photo);
+	saveMatData(lena_Deepcopy_Photo ,gray_Makeself_Photo , gray_Draw_Photo);
 
 	waitKey(0);
 
