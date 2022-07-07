80547fde8cc9fb3a81942c930dfc07afbef3f3e0
sk95120 <765871277@qq.com>
2022-07-07 10:19:00 +0800

    add homography decompse

--- 202204/OPENCV_CODE/20220629_CalibrateCamera01/20220629_CalibrateCamera01/CameraCalibrate03.cpp
+++ 202204/OPENCV_CODE/20220629_CalibrateCamera01/20220629_CalibrateCamera01/CameraCalibrate03.cpp
@@ -40,8 +40,8 @@ void calcChessboards(const Size &chessboardSize ,vector<Point3f> &corners) {
 }
 
 // 分解矩阵
-void decomposeMatrix(const Mat &mat1 , const Mat &mat2 ,const Mat &mat3 ,Size &patternSize 
-	,vector<Point3f> &corners1, vector<Point3f> &corners2, vector<Point3f> &corners3
+void decomposeMatrix(const Mat &mat1 , const Mat &mat2 ,Size &patternSize 
+	,vector<Point3f> &corners1, vector<Point3f> &corners2
 	,vector<Point2f> &h_input1 , vector<Point2f> &h_input2) {
 	String intrinsicsPath = samples::findFile("left_intrinsics.yml");
 	cout << "Camera samples intrinsics path: " << intrinsicsPath << endl;
@@ -60,9 +60,75 @@ void decomposeMatrix(const Mat &mat1 , const Mat &mat2 ,const Mat &mat3 ,Size &p
 	solvePnP(corners1 ,h_input1 ,cameraIntrinsicsMatrix ,cameraDistCoffes , rVecs_1, tVecs_1);
 	cout << "rVecs_1 " << endl << rVecs_1 << endl;
 	cout << "tVecs_1" << endl << tVecs_1 << endl;
-	cout << "h_input1 " << endl << h_input1 << endl;
 
+	Mat rVecs_2, tVecs_2;
+	solvePnP(corners2 ,h_input2 ,cameraIntrinsicsMatrix ,cameraDistCoffes ,rVecs_2 ,tVecs_2);
+	cout << "rVecs_2 " << endl << rVecs_2 << endl;
+	cout << "tVecs_2" << endl << tVecs_2 << endl;
 
+	// 求旋转矩阵R1 R1
+	Mat R1, R2, T1, T2;
+	Rodrigues(rVecs_1, R1);
+	Rodrigues(rVecs_2, R2);
+
+	// 求转换的矩阵（！）
+	Mat R1_to_R2, t1_to_t2;
+	R1_to_R2 = R2 * R1.t();
+	t1_to_t2 = R2 * (-R1.t()* tVecs_1) + tVecs_2;
+	cout << "R1_to_R2: " << endl << R1_to_R2 << endl;
+	cout << "t1_to_t2: " << endl << t1_to_t2 << endl;
+	Mat rvec1_to_rvec2;
+	Rodrigues(R1_to_R2 , rvec1_to_rvec2);
+
+	Mat normal = (Mat_<double>(3, 1) << 0, 0, 1);
+	Mat normal1 = R1 * normal;
+
+	Mat origin(3, 1, CV_64F, Scalar(0));
+	Mat origin1 = R1 * origin + tVecs_1;
+	double d_inv1 = 1.0 / normal1.dot(origin1);
+
+	// 计算单应矩阵
+	Mat homography_euclidean = R1_to_R2 + d_inv1 * t1_to_t2 * normal1.t();
+	cout << "homography euclidean (no normal) :" << endl << homography_euclidean << endl;
+	Mat homograph = cameraIntrinsicsMatrix * homography_euclidean * cameraIntrinsicsMatrix.t();
+	cout << "homography (no normal) :" << endl << homograph << endl;
+	homography_euclidean /= homography_euclidean.at<double>(2, 2);
+	homograph /= homograph.at<double>(2, 2);
+	cout << "homography euclidean :" << endl << homography_euclidean << endl;
+	cout << "homography :" << endl << homograph << endl;
+
+	//对比结果
+	Mat H_sample_1 = findHomography(corners1 ,corners3);
+	vector<Mat> r_decomp,t_decomp, normal_decomp;
+	int solutions = decomposeHomographyMat(H_sample_1 ,cameraIntrinsicsMatrix ,r_decomp ,t_decomp ,normal_decomp);
+	cout << "Decompse homography matrix estimated by findHomograpy() using (photo1,photo2):" << endl;
+	for (int i = 0; i < solutions ; i++) {
+		double factor_d1 = 1.0 / d_inv1;
+		Mat r_mat_decompse;
+		Rodrigues(r_decomp[i],r_mat_decompse);
+		cout << "soulution " << i << " :" <<endl;
+		cout << "rvec from homography decomposition:" << r_mat_decompse.t() << endl;
+		cout << "rvec from camera displacement:" << rvec1_to_rvec2.t() << endl;
+		cout << "tvec from homography decomposition:" << t_decomp[i].t() << " and scale by d:" << factor_d1 * t_decomp[i].t() << endl;
+		cout << "tvec from camera displacement:" << t1_to_t2.t() << endl;
+		cout << "plane normal from homography decomposition" << normal_decomp[i].t() << endl;
+		cout << "plane normal at camera 1 pose;"<< normal1.t() << endl;
+	}
+
+	solutions = decomposeHomographyMat(homograph ,cameraIntrinsicsMatrix ,r_decomp ,t_decomp ,normal_decomp);
+	cout << "Decompse homography matrix computed from the camera displacement:" << endl;
+	for (int i = 0; i < solutions; i++) {
+		double factor_d1 = 1.0 / d_inv1;
+		Mat r_mat_decompse;
+		Rodrigues(r_decomp[i], r_mat_decompse);
+		cout << "soulution " << i << " :" << endl;
+		cout << "rvec from homography decomposition:" << r_mat_decompse.t() << endl;
+		cout << "rvec from camera displacement:" << rvec1_to_rvec2.t() << endl;
+		cout << "tvec from homography decomposition:" << t_decomp[i].t() << " and scale by d:" << factor_d1 * t_decomp[i].t() << endl;
+		cout << "tvec from camera displacement:" << t1_to_t2.t() << endl;
+		cout << "plane normal from homography decomposition" << normal_decomp[i].t() << endl;
+		cout << "plane normal at camera 1 pose;" << normal1.t() << endl;
+	}
 
 }
 
@@ -129,10 +195,7 @@ int main(int argc ,char** agrv) {
 	calcChessboards(patternSize, circle_Photo_Corners_3f_3);
 
 	// 由相机内参、外参，计算空间点的单应矩阵H / PnP计算
-	vector<Point2f> homography_solve_input1;
-	vector<Point2f> homography_solve_input2;
-
-	decomposeMatrix(circle_Photo1 ,circle_Photo2 ,circle_Photo3 ,patternSize ,circle_Photo_Corners_3f_1 , circle_Photo_Corners_3f_2, circle_Photo_Corners_3f_3 ,homography_solve_input1 ,homography_solve_input2);
+	decomposeMatrix(circle_Photo1, circle_Photo2 ,patternSize, circle_Photo_Corners_3f_1, circle_Photo_Corners_3f_2 ,circle_Photo_Corners1, circle_Photo_Corners2);
 
 	return 0;
 }
