8282fa6097e70f7d9c611538734afa5c5f7c7f86
sk95120 <765871277@qq.com>
2022-07-25 17:07:26 +0800

    update fpspecial filter

--- 202204/MATLAB_CODES/digital_image_processing_02_demo_fpspecial.m
+++ 202204/MATLAB_CODES/digital_image_processing_02_demo_fpspecial.m
@@ -8,9 +8,18 @@ imshow(f);
 g1 = imfilter(f ,w ,'replicate');
 figure,imshow(g1 ,[]);
 
-f2 = tofloat(f);
+f2 = im2double(f);
 g2 = imfilter(f2 ,w ,'replicate');
 g = f2 - g2;
 figure,imshow(g);
 
 %%
+f = imread('.\\data\\Fig0228(a).tif');
+w4 = fspecial('laplacian' ,0);
+w8 = [1 ,1 ,1 ; 1 , -8 ,1 ; 1, 1, 1];
+f = im2double(f);
+g4 = f - imfilter(f ,w4 ,'replicate');
+g8 = f - imfilter(f ,w8 ,'replicate');
+imshow(f);
+figure,imshow(g4);
+figure,imshow(g8);
