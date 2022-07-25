f91545d87b8106701be70b53a08f6b1aee62241e
sk95120 <765871277@qq.com>
2022-07-25 16:38:27 +0800

    update photo read methods

--- 202204/MATLAB_CODES/digital_image_processing_01.m
+++ 202204/MATLAB_CODES/digital_image_processing_01.m
@@ -1,2 +1,8 @@
-lena = imread('..\\..\\202008\\opencv_project\\lenna\\lenna.jpg');
-imshow(lena);
+% show photo
+lena = imread('..\\..\\202008\\opencv_project\\lenna\\lenna_head_jpg_type.jpg');
+g = figure,imshow(lena);
+close(g);
+
+% write photo
+% imwrite(lena ,'..\\..\\202008\\opencv_project\\lenna\\lenna_head_jpg_type2.jpg');
+
