998b755549efb5fdb03eae448032f499ad83c65c
sk95120 <765871277@qq.com>
2022-07-28 14:46:52 +0800

    add replicate watershed matlab file

--- 202204/MATLAB_CODES/demo_watershed.m
+++ 202204/MATLAB_CODES/demo_watershed.m
@@ -8,3 +8,15 @@ L = watershed(-D);
 w = L == 0;
 g2 = f & ~w;
 figure ,imshow(g2);
+
+%%
+% 使用梯度的分水岭分割
+f = imread('./data/Fig0219(a).tif');
+imshow(f);
+h = fspecial('sobel');
+fd = im2double(f);
+g = sqrt(imfilter(fd ,h ,'replicate').^2 + imfilter(fd ,h','replicate').^2);
+figure,imshow(g);
+L = watershed(g);
+wr = L == 0;
+figure,imshow(wr);
