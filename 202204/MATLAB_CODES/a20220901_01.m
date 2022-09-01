ed3d7766e1d675556ba8402fa669eb7cc9e22446
sk95120 <765871277@qq.com>
2022-09-01 10:50:32 +0800

    upload dump directory

--- 202204/MATLAB_CODES/a20220901_01.m
+++ 202204/MATLAB_CODES/a20220901_01.m
@@ -1,4 +1,15 @@
 %% imshow 
 clc 
 clear
+
 I =  imread('.\data\matlab_book_example_picture\dipum_images_ch02\Fig0203(a)(chest-xray).tif');
+figure,subplot(121),imshow(I),subplot(122),imhist(I)
+axis tight %% axis number a^10 values
+
+%% dynamic range low
+%% class(I)
+%% min(I(:))
+%% max(I(:))
+figure,imshow(I,[])
+
+
