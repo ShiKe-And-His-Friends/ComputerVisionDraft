94cf2847df67eabd56edc71e802cfa93d202a7e2
sk95120 <765871277@qq.com>
2022-09-16 00:32:04 +0800

    增加Pytorch的GPU代码 20220916 庆祝Pytorch加入Linux基金会开源，我用欣英伟达显卡1080Ti搭建环境

--- 202204/220422.py
+++ 202204/220422.py
@@ -1,12 +1,21 @@
 import tensorflow as tf
 import torch
+import os
 
 '''
-    cmd nvidia-smi 
+    Command message: tensorflow/core/platform/cpu_feature_guard.cc:193] This TensorFlow binary is optimized with oneAPI Deep Neural Network Library (oneDNN) to use the following CPU instructions in performance-critical operations:  AVX AVX2
+To enable them in other operations, rebuild TensorFlow with the appropriate compiler flags.
+    Solve : Cpp complier message
+'''
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+'''
+    CMD nvidia-smi 
 '''
 
 x = torch.rand(5 ,3)
 print(x)
+print(x * x)
 print(torch.cuda.is_available())
 
 print("Hello Tensor again")
