a1514cbc5694aa3c574afafa1815db80810cae33
sk95120 <765871277@qq.com>
2022-09-27 10:26:27 +0800

    use compile tag that avoid offcial VTK opengl crash.

--- 202204/PCL_CODES/Learning/06_range_image_create.cxx
+++ 202204/PCL_CODES/Learning/06_range_image_create.cxx
@@ -1,2 +1,7 @@
-// PCL 1.12.1 makes crash
-// range_image_border_widght = pcl::visualization::RangeImageVisualizer::getRangeImageBorderWidget(range_image ,...);
+/*********** ERROR *************
+	PCL 1.12.1 makes crash
+	range_image_border_widght = pcl::visualization::RangeImageVisualizer::getRangeImageBorderWidget(range_image ,...);
+
+	#include <vtkAutoInit.h>
+	VTK_MODULE_INIT(vtkRenderingContextOpenGL2)
+ **/
