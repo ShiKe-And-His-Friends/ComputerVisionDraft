0c40230b9a51472883c55c1e6241b6832d997ede
sk95120 <765871277@qq.com>
2022-10-14 11:28:47 +0800

    update registion implicit shape model codes

--- 202204/PCL_CODES/Learning/13_recognition_implicit_shape_model.cxx
+++ 202204/PCL_CODES/Learning/13_recognition_implicit_shape_model.cxx
@@ -20,7 +20,7 @@
 	4. 对每一个特征点计算， 类别的vote权重
 	5. 上述计算结果的作为一个方向集，预测中心和能量
 
-	./implicit_shape_model.exe 
+	./implicit_shape_model.exe
 		ism_train_cat.pcd 0
 		ism_train_horse.pcd 1
 		ism_train_lioness.pcd 2
@@ -43,17 +43,17 @@
 #include <boost/thread/thread.hpp>
 #include <pcl/visualization/pcl_visualizer.h>
 
-int main(int argc ,char ** argv) {
+int main(int argc, char** argv) {
 
 	std::cout << "Recognition implicit shape model." << std::endl;
 
-	if (argc == 0 || argc %2 == 0) {
+	if (argc == 0 || argc % 2 == 0) {
 		std::cout << "input arguments not match." << std::endl;
 		return (-1);
 	}
 	unsigned int number_of_training_clouds = (argc - 3) / 2;
 	pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimator;
-	normal_estimator.setRadiusSearch(2.50f);
+	normal_estimator.setRadiusSearch(25.0);
 
 	std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> training_clouds;
 	std::vector<pcl::PointCloud<pcl::Normal>::Ptr> training_normals;
@@ -62,38 +62,43 @@ int main(int argc ,char ** argv) {
 	// 提取点云特征
 	for (size_t i_cloud = 0; i_cloud < number_of_training_clouds - 1; i_cloud++) {
 		pcl::PointCloud<pcl::PointXYZ>::Ptr tr_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-		if (pcl::io::loadPCDFile<pcl::PointXYZ>(argv[i_cloud *2 + 1] ,*tr_cloud) == -1) {
-			std::cout << "load pcd file failure: " << argv[i_cloud*2 +1] << std::endl;
+		if (pcl::io::loadPCDFile<pcl::PointXYZ>(argv[i_cloud * 2 + 1], *tr_cloud) == -1) {
+			std::cout << "load pcd file failure: " << argv[i_cloud * 2 + 1] << std::endl;
 			return -2;
 		}
 		pcl::PointCloud<pcl::Normal>::Ptr tr_normals = (new pcl::PointCloud<pcl::Normal>)->makeShared();
 		normal_estimator.setInputCloud(tr_cloud);
 		normal_estimator.compute(*tr_normals);
 
-		unsigned int tr_class = static_cast<unsigned int>(strtol(argv[i_cloud*2+2] ,0 ,10)); //strtol str->long
+		unsigned int tr_class = static_cast<unsigned int>(strtol(argv[i_cloud * 2 + 2], 0, 10)); //strtol str->long
 		training_clouds.push_back(tr_cloud);
 		training_normals.push_back(tr_normals);
 		training_classes.push_back(tr_class);
 	}
 
 	// 用法线特征提取FPHF快速特征直方图 计算法线夹角
-	pcl::FPFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::Histogram<153>>::Ptr fpfh (new pcl::FPFHEstimation<pcl::PointXYZ ,pcl::Normal ,pcl::Histogram<153>>);
-	fpfh->setRadiusSearch(3.0f);
-	pcl::Feature < pcl::PointXYZ, pcl::Histogram<153>>::Ptr feature_estimator(fpfh);
-	pcl::ism::ImplicitShapeModelEstimation<153, pcl::PointXYZ, pcl::Normal> ism; //特征估计器
+	const int Histogram_Size_ = 153; // 153
+	pcl::FPFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::Histogram<Histogram_Size_>>::Ptr fpfh(new pcl::FPFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::Histogram<Histogram_Size_>>);
+	fpfh->setRadiusSearch(30.0);
+	pcl::Feature < pcl::PointXYZ, pcl::Histogram<Histogram_Size_>>::Ptr feature_estimator(fpfh);
+	pcl::ism::ImplicitShapeModelEstimation<Histogram_Size_, pcl::PointXYZ, pcl::Normal> ism; //特征估计器
 	ism.setFeatureEstimator(feature_estimator);
 	ism.setTrainingClouds(training_clouds);
 	ism.setTrainingNormals(training_normals);
 	ism.setTrainingClasses(training_classes);
 	ism.setSamplingSize(2.0f);
 
-	pcl::ism::ImplicitShapeModelEstimation<153, pcl::PointXYZ, pcl::Normal>::ISMModelPtr model = std::shared_ptr<pcl::features::ISMModel>(new pcl::features::ISMModel); //模型
-	ism.trainISM(model); //训练模型
-
+	pcl::ism::ImplicitShapeModelEstimation<Histogram_Size_, pcl::PointXYZ, pcl::Normal>::ISMModelPtr model = std::shared_ptr<pcl::features::ISMModel>(new pcl::features::ISMModel); //模型
 	std::string file("train_ism_model.txt");
-	model->saveModelToFile(file);
-	model->loadModelFromfile(file);
-
+	if (!model->loadModelFromfile(file)) {
+		ism.trainISM(model); //训练模型
+		model->saveModelToFile(file);
+		model->loadModelFromfile(file);
+	}
+	else {
+		std::cout << "Load model file." << std::endl;
+	}
+	
 	// 载入测试点云
 	unsigned int testing_class = static_cast<unsigned int>(strtol(argv[argc - 1], 0, 10));
 	pcl::PointCloud<pcl::PointXYZ>::Ptr testing_cloud(new pcl::PointCloud<pcl::PointXYZ>);
@@ -101,6 +106,7 @@ int main(int argc ,char ** argv) {
 		std::cout << "load pcd file failure: " << argv[argc - 2] << std::endl;
 		return -4;
 	}
+	std::cout << "load test pcd file success: " << argv[argc - 2] << std::endl;
 	pcl::PointCloud<pcl::Normal>::Ptr testing_normals = (new pcl::PointCloud<pcl::Normal>)->makeShared();
 	normal_estimator.setInputCloud(testing_cloud);
 	normal_estimator.compute(*testing_normals);
@@ -117,7 +123,8 @@ int main(int argc ,char ** argv) {
 	double radius = model->sigmas_[testing_class] * 10.0;
 	double sigma = model->sigmas_[testing_class];
 	std::vector<pcl::ISMPeak, Eigen::aligned_allocator<pcl::ISMPeak>> strongest_peaks;
-	vote_list->findStrongestPeaks(strongest_peaks ,testing_class ,radius ,sigma);
+	vote_list->findStrongestPeaks(strongest_peaks, testing_class, radius, sigma);
+	std::cout << "Strongest peaks " << strongest_peaks.size() << std::endl;
 
 	pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = (new pcl::PointCloud<pcl::PointXYZRGB>)->makeShared();
 	colored_cloud->height = 0;
@@ -127,7 +134,7 @@ int main(int argc ,char ** argv) {
 	point.g = 255;
 	point.b = 255;
 
-	for (size_t i_point = 0; i_point < testing_cloud->points.size();i_point ++) {
+	for (size_t i_point = 0; i_point < testing_cloud->points.size(); i_point++) {
 		point.x = testing_cloud->points[i_point].x;
 		point.y = testing_cloud->points[i_point].y;
 		point.z = testing_cloud->points[i_point].z;
@@ -146,11 +153,16 @@ int main(int argc ,char ** argv) {
 	colored_cloud->height += strongest_peaks.size();
 
 	// 视图查看
-	pcl::visualization::CloudViewer viewer("Result viewer");
-	viewer.showCloud(colored_cloud);
+	pcl::visualization::PCLVisualizer viewer("Result viewer");
+	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> colorh(testing_cloud ,30 ,200,30);
+	viewer.addPointCloud(testing_cloud ,colorh ,"test_data");
+	viewer.addPointCloud(colored_cloud ,"centors");
+	viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE ,10 ,"test_data");
+	viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "centors");
+
 	while (!viewer.wasStopped()) {
-
-
+		viewer.spinOnce(10);
+		boost::this_thread::sleep(boost::posix_time::microseconds(1000000));
 	}
 	return 0;
 }
