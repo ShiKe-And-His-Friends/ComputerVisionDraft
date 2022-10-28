61f485c94ff0a3eef457ef47b663a34752a9b1db
sk95120 <765871277@qq.com>
2022-10-28 15:29:17 +0800

    matlibplot line color codes

--- 202210/torch/yolo/utils/callback.py
+++ 202210/torch/yolo/utils/callback.py
@@ -58,7 +58,7 @@ class LossHistory():
             else:
                 num = 15
             plt.plot(iters,savgol_filter(self.losses ,num ,3),'green' ,linstyle= '--' ,label = 'smooth train loss')
-            plt.plot(iters,savgol_filter(self.val_loss ,num ,3) ,label = 'smooth val loss')
+            plt.plot(iters,savgol_filter(self.val_loss ,num ,3) ,'red' ,linstyle= '--' ,label = 'smooth val loss')
         except:
             print("Exception: loss plot methods")
             pass
