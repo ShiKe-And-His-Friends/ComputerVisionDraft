6a3e297f5a883c8fc4a6ed0c32351f44834c954d
sk95120 <765871277@qq.com>
2022-10-28 15:50:33 +0800

    change parameters of peroid

--- 202210/torch/yolo/train.py
+++ 202210/torch/yolo/train.py
@@ -126,7 +126,7 @@ if __name__ == '__main__':
     optimizer_type = "sgd"
     momentum = 0.937
     weight_decay = 5e-4
-    save_period = 15 #多少次epoch保存一次权值
+    save_period = 5 #多少次epoch保存一次权值
     input_shape = [416, 416]
     anchors_mask = [[6,7,8] ,[3,4,5] ,[0,1,2]] #用于帮助代码找到对应的先验框，一般不修改
 
@@ -135,7 +135,7 @@ if __name__ == '__main__':
 
     save_dir = 'logs' # 保存权值和日志文件
     eval_flag = True # 是否训练时评估，评估对象为验证集
-    eval_period = 10 # 多少次epoch评估一次，不建议频繁。获得验证集的mAP和get_map.py稍有不同,参数更加保守.
+    eval_period = 5 # 多少次epoch评估一次，不建议频繁。获得验证集的mAP和get_map.py稍有不同,参数更加保守.
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # *********************************************************#
