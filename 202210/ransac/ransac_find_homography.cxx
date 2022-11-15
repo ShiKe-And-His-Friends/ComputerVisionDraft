174f680fb1539205eccc87d81fb55abd9d829951
sk95120 <765871277@qq.com>
2022-11-15 16:21:25 +0800

    upload py_findhomography cpp codes

--- 202210/ransac/ransac_find_homography.cxx
+++ 202210/ransac/ransac_find_homography.cxx
@@ -9,7 +9,91 @@
 
 using namespace cv;
 
+void verify_pygcransac(std::vector<KeyPoint> kps1,
+	std::vector<KeyPoint> kps2,
+	std::vector<DMatch> tentatives,
+	int h1,int w1 ,
+	int h2,int w2 ,
+	int sampler_id)
+{
+	std::vector<double> correspindences;
+	for (int i = 0; i < tentatives.size(); i++) {
+		correspindences.push_back(kps1.at(tentatives[i].queryIdx).pt.x);
+		correspindences.push_back(kps2.at(tentatives[i].trainIdx).pt.x);
+		correspindences.push_back(kps1.at(tentatives[i].queryIdx).pt.y);
+		correspindences.push_back(kps2.at(tentatives[i].trainIdx).pt.y);	
+	}
 
+	double spatial_coherence_weight = 0.1;
+	double threshold = 0.5;
+	double conf = 0.5;
+	int max_iters = 9000;
+	int min_iters = 100;
+	bool use_sprt = false;
+	double min_inlier_ratio_for_sprt = 1;
+	int sampler = sampler_id;
+	int neighborhood = 0;
+	double neighborhood_size = 3;
+	bool use_space_partitioning = true;
+	double sampler_variance = 0.5;
+	int lo_number = 3;
+
+	size_t NUM_TENTS = correspindences.size();
+	size_t DIM = 2;
+
+
+	std::vector<double> probabilities;
+	std::vector<double> H(9);
+	std::vector<bool> inliers(NUM_TENTS);
+
+	int num_inl = 0; // Point correspondence-based fundamental matrix estimation
+	num_inl = findHomography_(
+		correspindences,
+		probabilities,
+		inliers,
+		H,
+		h1, w1, h2, w2,
+		spatial_coherence_weight,
+		threshold,
+		conf,
+		max_iters,
+		min_iters,
+		use_sprt,
+		min_inlier_ratio_for_sprt,
+		sampler,
+		neighborhood,
+		neighborhood_size,
+		use_space_partitioning,
+		sampler_variance,
+		lo_number);
+
+	std::cout << "H :" << std::endl;
+	for (int i = 1; i <= 9; i++) {
+		std::cout << H[i - 1] << " ";
+		if (i % 3 == 0) {
+			std::cout << " " << std::endl;
+		}
+	}
+
+	/****
+	py::array_t<bool> inliers_ = py::array_t<bool>(NUM_TENTS);
+	py::buffer_info buf3 = inliers_.request();
+	bool* ptr3 = (bool*)buf3.ptr;
+	for (size_t i = 0; i < NUM_TENTS; i++)
+		ptr3[i] = inliers[i];
+
+	if (num_inl == 0) {
+		return py::make_tuple(pybind11::cast<pybind11::none>(Py_None), inliers_);
+	}
+	py::array_t<double> H_ = py::array_t<double>({ 3,3 });
+	py::buffer_info buf2 = H_.request();
+	double* ptr2 = (double*)buf2.ptr;
+	for (size_t i = 0; i < 9; i++)
+		ptr2[i] = H[i];
+
+	return py::make_tuple(H_, inliers_);
+	****/
+}
 
 void draw_matches(std::vector<KeyPoint> kps1, std::vector<KeyPoint> kps2 ,std::vector<DMatch> matches , Mat img1 ,Mat img2 ,Mat H ,Mat mask) {
 	if (H.empty()){
